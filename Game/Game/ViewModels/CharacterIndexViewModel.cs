3af2bbf75379f0973304b913c62e379ef465d629
cbrod <broderickcel@seattleu.edu>
2020-02-09 18:29:09 -0800

    Updated CharacterIndexViewModel

--- Game/Game/ViewModels/CharacterIndexViewModel.cs
+++ Game/Game/ViewModels/CharacterIndexViewModel.cs
@@ -14,136 +14,136 @@ namespace Game.ViewModels
     /// </summary>
     public class CharacterIndexViewModel : BaseViewModel<CharacterModel>
     {
-        //#region Singleton
+        #region Singleton
 
-        //// Make this a singleton so it only exist one time because holds all the data records in memory
-        //private static volatile CharacterIndexViewModel instance;
-        //private static readonly object syncRoot = new Object();
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile CharacterIndexViewModel instance;
+        private static readonly object syncRoot = new Object();
 
-        //public static CharacterIndexViewModel Instance
-        //{
-        //    get
-        //    {
-        //        if (instance == null)
-        //        {
-        //            lock (syncRoot)
-        //            {
-        //                if (instance == null)
-        //                {
-        //                    instance = new CharacterIndexViewModel();
-        //                    instance.Initialize();
-        //                }
-        //            }
-        //        }
+        public static CharacterIndexViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new CharacterIndexViewModel();
+                            instance.Initialize();
+                        }
+                    }
+                }
 
-        //        return instance;
-        //    }
-        //}
+                return instance;
+            }
+        }
 
-        //#endregion Singleton
+        #endregion Singleton
 
-        //#region Constructor
+        #region Constructor
 
-        ///// <summary>
-        ///// Constructor
-        ///// 
-        ///// The constructor subscribes message listeners for crudi operations
-        ///// </summary>
-        //public CharacterIndexViewModel()
-        //{
-        //    Title = "Characters";
+        /// <summary>
+        /// Constructor
+        /// 
+        /// The constructor subscribes message listeners for crudi operations
+        /// </summary>
+        public CharacterIndexViewModel()
+        {
+            Title = "Characters";
 
-        //    #region Messages
+            #region Messages
 
-        //    // Register the Create Message
-        //    MessagingCenter.Subscribe<CharacterCreatePage, CharacterModel>(this, "Create", async (obj, data) =>
-        //    {
-        //        await CreateAsync(data as CharacterModel);
-        //    });
+            //    // Register the Create Message
+            //    MessagingCenter.Subscribe<CharacterCreatePage, CharacterModel>(this, "Create", async (obj, data) =>
+            //    {
+            //        await CreateAsync(data as CharacterModel);
+            //    });
 
-        //    // Register the Update Message
-        //    MessagingCenter.Subscribe<CharacterUpdatePage, CharacterModel>(this, "Update", async (obj, data) =>
-        //    {
-        //        // Have the item update itself
-        //        data.Update(data);
+            //    // Register the Update Message
+            //    MessagingCenter.Subscribe<CharacterUpdatePage, CharacterModel>(this, "Update", async (obj, data) =>
+            //    {
+            //        // Have the item update itself
+            //        data.Update(data);
 
-        //        await UpdateAsync(data as CharacterModel);
-        //    });
+            //        await UpdateAsync(data as CharacterModel);
+            //    });
 
-        //    // Register the Delete Message
-        //    MessagingCenter.Subscribe<CharacterDeletePage, CharacterModel>(this, "Delete", async (obj, data) =>
-        //    {
-        //        await DeleteAsync(data as CharacterModel);
-        //    });
+            //    // Register the Delete Message
+            //    MessagingCenter.Subscribe<CharacterDeletePage, CharacterModel>(this, "Delete", async (obj, data) =>
+            //    {
+            //        await DeleteAsync(data as CharacterModel);
+            //    });
 
-        //    // Register the Set Data Source Message
-        //    MessagingCenter.Subscribe<AboutPage, int>(this, "SetDataSource", async (obj, data) =>
-        //    {
-        //        await SetDataSource(data);
-        //    });
+            //    // Register the Set Data Source Message
+            //    MessagingCenter.Subscribe<AboutPage, int>(this, "SetDataSource", async (obj, data) =>
+            //    {
+            //        await SetDataSource(data);
+            //    });
 
-        //    // Register the Wipe Data List Message
-        //    MessagingCenter.Subscribe<AboutPage, bool>(this, "WipeDataList", async (obj, data) =>
-        //    {
-        //        await WipeDataListAsync();
-        //    });
+            //    // Register the Wipe Data List Message
+            //    MessagingCenter.Subscribe<AboutPage, bool>(this, "WipeDataList", async (obj, data) =>
+            //    {
+            //        await WipeDataListAsync();
+            //    });
 
-        //    #endregion Messages
-        //}
+            #endregion Messages
+        }
 
-        //#endregion Constructor
-        
-        //#region DataOperations_CRUDi
+        #endregion Constructor
 
-        ///// <summary>
-        ///// Returns the item passed in
-        ///// </summary>
-        ///// <param name="data"></param>
-        ///// <returns></returns>
-        //public CharacterModel CheckIfItemExists(CharacterModel data)
-        //{
-        //    // This will walk the items and find if there is one that is the same.
-        //    // If so, it returns the item...
+        #region DataOperations_CRUDi
 
-        //    var myList = Dataset.Where(a =>
-        //                                a.Name == data.Name)
-        //                                .FirstOrDefault();
+        /// <summary>
+        /// Returns the character passed in
+        /// </summary>
+        /// <param name="data"></param>
+        /// <returns></returns>
+        public CharacterModel CheckIfItemExists(CharacterModel data)
+        {
+            // This will walk the characters and find if there is one that is the same.
+            // If so, it returns the character...
 
-        //    if (myList == null)
-        //    {
-        //        // it's not a match, return false;
-        //        return null;
-        //    }
+            var myList = Dataset.Where(a =>
+                                        a.Name == data.Name)
+                                        .FirstOrDefault();
 
-        //    return myList;
-        //}
+            if (myList == null)
+            {
+                // it's not a match, return false;
+                return null;
+            }
+
+            return myList;
+        }
 
         ///// <summary>
         ///// Load the Default Data
         ///// </summary>
         ///// <returns></returns>
-        //public override List<CharacterModel> GetDefaultData() 
+        //public override List<CharacterModel> GetDefaultData()
         //{
         //    return DefaultData.LoadData(new CharacterModel());
         //}
 
-        //#endregion DataOperations_CRUDi
+        #endregion DataOperations_CRUDi
 
-        //#region SortDataSet
+        #region SortDataSet
 
-        ///// <summary>
-        ///// The Sort Order for the CharacterModel
-        ///// </summary>
-        ///// <param name="dataset"></param>
-        ///// <returns></returns>
-        //public override List<CharacterModel> SortDataset(List<CharacterModel> dataset)
-        //{
-        //    return dataset
-        //            .OrderBy(a => a.Name)
-        //            .ThenBy(a => a.Description)
-        //            .ToList();
-        //}
+        /// <summary>
+        /// The Sort Order for the CharacterModel
+        /// </summary>
+        /// <param name="dataset"></param>
+        /// <returns></returns>
+        public override List<CharacterModel> SortDataset(List<CharacterModel> dataset)
+        {
+            return dataset
+                    .OrderBy(a => a.Name)
+                    .ThenBy(a => a.Description)
+                    .ToList();
+        }
 
-        //#endregion SortDataSet
+        #endregion SortDataSet
     }
 }
