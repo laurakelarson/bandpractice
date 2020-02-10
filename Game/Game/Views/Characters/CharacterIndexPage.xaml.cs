12d3c1555649d4ebcd5478734976feea0ae29c1c
cbrod <broderickcel@seattleu.edu>
2020-02-09 20:58:22 -0800

    Added OnAppearing method to CharacterIndexPage

--- Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
+++ Game/Game/Views/Characters/CharacterIndexPage.xaml.cs
@@ -31,5 +31,29 @@ namespace Game.Views.Characters
         {
             //TODO hook up Create
         }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
     }
 }
