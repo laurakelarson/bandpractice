b56dfa4ee887d5cb4115172b56054ae6a9ac2f94
Laura Larson <larsonlaura@seattleu.edu>
2020-02-11 14:07:30 -0800

    Added Character service class to assign default image to characters

---
+++ Game/Game/Services/CharacterService.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Services
+{
+    static class CharacterService
+    {
+        // Return the Default Image URI for the Local Image for an Item.
+        public static string DefaultImageURI = "default_icon.png";
+    }
+}
