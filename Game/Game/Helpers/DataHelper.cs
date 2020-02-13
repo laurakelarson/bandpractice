7ca04a2424c44d8dae86e787387f9042abef7c22
cbrod <broderickcel@seattleu.edu>
2020-02-12 20:03:45 -0800

    DataHelper: add TambourinePlayer

--- Game/Game/Helpers/DataHelper.cs
+++ Game/Game/Helpers/DataHelper.cs
@@ -1,10 +1,35 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Models;
 
 namespace Game.Helpers
 {
     class DataHelper
     {
+
+        /// <summary>
+        /// Returns default Tambourine Player CharacterModel
+        /// </summary>
+        /// <returns></returns>
+        public CharacterModel DefaultTambourine()
+        {
+            return new CharacterModel
+            {
+                Name = "Tambourine Player",
+                Type = CharacterTypeEnum.TambourinePlayer,
+                ImageURI = "tambourine.png",
+                IconURI = "tambourine_icon.png",
+                Alive = true,
+                Level = 1,
+                TotalExperience = 0,
+                Attack = 1,
+                Defense = 1,
+                Speed = 1,
+                MaxHealth = 20,
+                CurrentHealth = 20
+            };
+        }
+
     }
 }
