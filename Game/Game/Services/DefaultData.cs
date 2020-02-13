42220149b442cab613ef5b1914ab76feb66ef9de
cbrod <broderickcel@seattleu.edu>
2020-02-12 21:22:09 -0800

    DefaultData: added items

--- Game/Game/Services/DefaultData.cs
+++ Game/Game/Services/DefaultData.cs
@@ -17,36 +17,225 @@ namespace Game.Services
         {
             var datalist = new List<ItemModel>()
             {
-                new ItemModel {
-                    Name = "Gold Sword",
-                    Description = "Sword made of Gold, really expensive looking",
-                    ImageURI = "http://www.clker.com/cliparts/e/L/A/m/I/c/sword-md.png",
-                    Range = 0,
-                    Damage = 9,
-                    Value = 9,
-                    Location = ItemLocationEnum.PrimaryHand,
-                    Attribute = AttributeEnum.Defense},
-
-                new ItemModel {
-                    Name = "Strong Shield",
-                    Description = "Enough to hide behind",
-                    ImageURI = "http://www.clipartbest.com/cliparts/4T9/LaR/4T9LaReTE.png",
+                new ItemModel
+                {
+                    Name = "Earplugs",
+                    Description = "Squishy foam earplugs that help block noise",
+                    ImageURI = "item_earplugs.png",
                     Range = 0,
                     Damage = 0,
-                    Value = 9,
-                    Location = ItemLocationEnum.OffHand,
-                    Attribute = AttributeEnum.Defense},
-
-                new ItemModel {
-                    Name = "Bunny Hat",
-                    Description = "Pink hat with fluffy ears",
-                    ImageURI = "http://www.clipartbest.com/cliparts/yik/e9k/yike9kMyT.png",
-                    Range = 0,
-                    Damage = 0,
-                    Value = 9,
+                    Value = 2,
                     Location = ItemLocationEnum.Head,
-                    Attribute = AttributeEnum.Speed},
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Earmuffs",
+                    Description = "Cute, fluffy earmuffs that block a little noise",
+                    ImageURI = "item_earmuffs.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.Head,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Noise-Canceling Headphones",
+                    Description = "Powerful noise-canceling headphones that block noise",
+                    ImageURI = "item_headphones.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 3,
+                    Location = ItemLocationEnum.Head,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Microphone",
+                    Description = "A battery-powered microphone that amplifies the band’s music",
+                    ImageURI = "item_microphone.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 2,
+                    Location = ItemLocationEnum.OffHand,
+                    Attribute = AttributeEnum.Attack
+                },
+
+                new ItemModel
+                {
+                    Name = "Coffee",
+                    Description = "A slightly bitter caffeinated beverage",
+                    ImageURI = "item_coffee.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.OffHand,
+                    Attribute = AttributeEnum.Speed
+                },
+
+                new ItemModel
+                {
+                    Name = "Energy Drink",
+                    Description = "A carbonated caffeinated beverage with a weird aftertaste",
+                    ImageURI = "item_energy_drink.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.OffHand,
+                    Attribute = AttributeEnum.Speed
+                },
+
+                new ItemModel
+                {
+                    Name = "Metronome",
+                    Description = "A handheld metronome that keeps the band’s beat",
+                    ImageURI = "item_metronome.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.OffHand,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Tuning Fork",
+                    Description = "A tuning fork that helps the band keep its pitch",
+                    ImageURI = "item_tuning_fork.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.OffHand,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Band T-shirt",
+                    Description = "A soft t-shirt featuring the band’s logo",
+                    ImageURI = "item_band_shirt.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.Necklass,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Band Hoodie",
+                    Description = "A comfy hoodie featuring the band’s logo",
+                    ImageURI = "item_band_hoodie.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 2,
+                    Location = ItemLocationEnum.Necklass,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Cool Outfit",
+                    Description = "A cool outfit that looks effortlessly hip (like a rock star would wear)",
+                    ImageURI = "item_cool_outfit.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 3,
+                    Location = ItemLocationEnum.Necklass,
+                    Attribute = AttributeEnum.Defense
+                },
+
+                new ItemModel
+                {
+                    Name = "Ring",
+                    Description = "A simple, nondescript ring",
+                    ImageURI = "item_ring.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.LeftFinger,
+                    Attribute = AttributeEnum.Attack
+                },
+
+                new ItemModel
+                {
+                    Name = "Mood Ring",
+                    Description = "A ring with a stone that changes color depending on the wearer’s mood",
+                    ImageURI = "item_mood_ring.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 2,
+                    Location = ItemLocationEnum.LeftFinger,
+                    Attribute = AttributeEnum.Attack
+                },
+
+                new ItemModel
+                {
+                    Name = "Temporary Tattoo",
+                    Description = "A temporary tattoo of the band’s lyrics written around the wearer’s finger like a ring",
+                    ImageURI = "item_temporary_tattoo.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 3,
+                    Location = ItemLocationEnum.LeftFinger,
+                    Attribute = AttributeEnum.Attack
+                },
+
+                new ItemModel
+                {
+                    Name = "Athletic Socks",
+                    Description = "Simple athletic socks",
+                    ImageURI = "item_athletic_socks.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.Feet,
+                    Attribute = AttributeEnum.Attack
+                },
+
+                new ItemModel
+                {
+                    Name = "Lucky Socks",
+                    Description = "Socks that feature a funky pattern",
+                    ImageURI = "item_lucky_socks.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 2,
+                    Location = ItemLocationEnum.Feet,
+                    Attribute = AttributeEnum.Attack
+                },
+
+                new ItemModel
+                {
+                    Name = "Comfy Sneakers",
+                    Description = "Sneakers that are comfortable to walk and stand in",
+                    ImageURI = "item_comfy_sneakers.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 1,
+                    Location = ItemLocationEnum.Feet,
+                    Attribute = AttributeEnum.Speed
+                },
+
+                new ItemModel
+                {
+                    Name = "Bunny Slippers",
+                    Description = "Fluffy slippers that look like bunnies",
+                    ImageURI = "item_bunny_slippers.png",
+                    Range = 0,
+                    Damage = 0,
+                    Value = 2,
+                    Location = ItemLocationEnum.Feet,
+                    Attribute = AttributeEnum.Speed
+                }
+
             };
+
+
 
             return datalist;
         }
