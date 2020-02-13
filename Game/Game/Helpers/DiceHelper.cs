36457fa8841b3240b0701d03096f67b2b28c56b2
Laura Larson <larsonlaura@seattleu.edu>
2020-02-13 14:45:04 -0800

    Added DiceHelper class

---
+++ Game/Game/Helpers/DiceHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Helpers
+{
+    class DiceHelper
+    {
+        Random r = new Random();
+
+        public int diceRoller(int sides)
+        {
+            var roll = r.Next(1, sides);
+            return roll;
+        }
+    }
+}
