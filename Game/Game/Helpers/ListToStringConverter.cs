af96e9cb92884e5f2147d4e317567ce183b9147b
cbrod <broderickcel@seattleu.edu>
2020-02-13 12:33:35 -0800

    Adding list to string converter to project file

---
+++ Game/Game/Helpers/ListToStringConverter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace Game.Helpers
+{
+    class ListToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            //            return string.Join(",", value);
+            return "item";
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
