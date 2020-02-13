1fef986b5b0e36578e49210a6f68efa5bcc5e83a
Laura Larson <larsonlaura@seattleu.edu>
2020-02-13 07:05:09 -0800

    Added MonsterService

---
+++ Game/Game/Services/MonsterService.cs
@@ -0,0 +1,8 @@
+﻿namespace Game.Services
+{
+    static class MonsterService
+    {
+        // Return the Default Image URI for the Local Image for an Item.
+        public static string DefaultImageURI = "monster_airhorn.png";
+    }
+}
