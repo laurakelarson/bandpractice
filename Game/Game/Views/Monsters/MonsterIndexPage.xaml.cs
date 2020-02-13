96988f333b91e3a83da68dc2c31a842468f3516a
cbrod <broderickcel@seattleu.edu>
2020-02-12 23:08:55 -0800

    Adding MonsterIndexPage to project file

---
+++ Game/Game/Views/Monsters/MonsterIndexPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Threading.Tasks;
+using Game.Models;
+using Game.ViewModels;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views.Monsters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterIndexPage : ContentPage
+    {
+        // The view model, used for data binding
+        readonly MonsterIndexViewModel ViewModel;
+
+        /// <summary>
+        /// Constructor for Index Page
+        /// 
+        /// Get the MonsterIndexView Model
+        /// </summary>
+        public MonsterIndexPage()
+        {
+            InitializeComponent();
+
+            BindingContext = ViewModel = MonsterIndexViewModel.Instance;
+        }
+
+        /// <summary>
+        /// The row selected from the list
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        async void OnMonsterSelected(object sender, SelectedItemChangedEventArgs args)
+        {
+            MonsterModel data = args.SelectedItem as MonsterModel;
+            if (data == null)
+            {
+                return;
+            }
+
+            // Open the Read Page
+   //         await Navigation.PushAsync(new MonsterReadPage(new GenericViewModel<MonsterModel>(data)));
+
+            // Manually deselect item.
+            MonstersListView.SelectedItem = null;
+        }
+
+        /// <summary>
+        /// Call to Add a new record
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void AddMonster_Clicked(object sender, EventArgs e)
+        {
+  //          await Navigation.PushModalAsync(new NavigationPage(new MonsterCreatePage(new GenericViewModel<MonsterModel>())));
+
+        }
+
+        /// <summary>
+        /// Refresh the list on page appearing
+        /// </summary>
+        protected override void OnAppearing()
+        {
+            base.OnAppearing();
+
+            BindingContext = null;
+
+            // If no data, then set it for needing refresh
+            if (ViewModel.Dataset.Count == 0)
+            {
+                ViewModel.SetNeedsRefresh(true);
+            }
+
+            // If the needs Refresh flag is set update it
+            if (ViewModel.NeedsRefresh())
+            {
+                ViewModel.LoadDatasetCommand.Execute(null);
+            }
+
+            BindingContext = ViewModel;
+        }
+    }
+}
