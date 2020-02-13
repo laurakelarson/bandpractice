125b1c4d84da1ee6d8d7f05ecd029e6e2a036492
Laura Larson <larsonlaura@seattleu.edu>
2020-02-13 08:12:33 -0800

    Added monster type enum for use in monster helper

---
+++ Game/Game/Models/Enum/MonsterTypeEnum.cs
@@ -0,0 +1,32 @@
+﻿namespace Game.Models
+{
+    /// <summary>
+    /// Available monster types
+    /// </summary>
+    public enum MonsterTypeEnum
+    {
+        Chomper,
+        MassiveStatic,
+        Motobeast,
+        Kazoom,
+        Panpot,
+        Jackhammer,
+        Brakez,
+        Driller,
+        Alarmer,
+        ShrillBabe,
+        BuzzRowdy,
+        PiercingFeedback,
+        FranDrescher,
+        YowlingFeline,
+        Nickelback,
+        LloydChristmas,
+        RecorderApprentice,
+        AirhornLeviathan,
+        EarsplittingChalkboard,
+        RubberChickenBlob,
+        AgonizingSilence,
+        GilbertGottfried
+
+    }
+}
