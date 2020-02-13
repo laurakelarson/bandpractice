2f71277ede1601ff47f3287d332a1ebe616a03bb
Laura Larson <larsonlaura@seattleu.edu>
2020-02-13 15:10:24 -0800

    Added Monster read page and updated xaml view

---
+++ Game/Game/Views/Monsters/MonsterReadPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using Game.Models;
+using Game.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace Game.Views.Monsters
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class MonsterReadPage : ContentPage
+    {
+        // View Model for item
+        readonly GenericViewModel<MonsterModel> ViewModel;
+
+        /// <summary>
+        /// Constructor called with a view model
+        /// This is the primary way to open the page
+        /// The viewModel is the data that should be displayed
+        /// </summary>
+        /// <param name="viewModel"></param>
+        public MonsterReadPage(GenericViewModel<MonsterModel> data)
+        {
+            InitializeComponent();
+
+            BindingContext = this.ViewModel = data;
+        }
+
+        /// <summary>
+        /// Save calls to Update
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        async void Update_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new NavigationPage(new MonsterUpdatePage(new GenericViewModel<MonsterModel>(ViewModel.Data))));
+            //await Navigation.PopAsync();
+        }
+
+        async void Delete_Clicked(object sender, EventArgs e)
+        {
+            //await Navigation.PushModalAsync(new NavigationPage(new MonsterDeletePage(new GenericViewModel<MonsterModel>(ViewModel.Data))));
+            //await Navigation.PopAsync();
+        }
+    }
+}
