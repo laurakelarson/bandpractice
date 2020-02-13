423e6755cad62a750367af689a4f7cd65eb2c05f
Alex Peterson <petersonalex@seattleu.edu>
2020-02-13 13:50:36 -0800

    Add ItemTypes to ItemTypeEnum.

--- Game/Game/Models/Enum/ItemTypeEnum.cs
+++ Game/Game/Models/Enum/ItemTypeEnum.cs
@@ -6,6 +6,36 @@ namespace Game.Models.Enum
 {
     public enum ItemTypeEnum
     {
-
+        Earplugs,
+        Earmuffs,
+        NoiseCancelingHeadphones,
+        Microphone,
+        Coffee,
+        EnergyDrink,
+        Metronome,
+        TuningFork,
+        BandTshirt,
+        BandHoodie,
+        CoolOutfit,
+        Ring,
+        MoodRing,
+        TemporaryTattoo,
+        AthleticSocks,
+        LuckySocks,
+        ComfySneakers,
+        BunnySlippers,
+        Triangle,
+        PrankDoorbell,
+        WhoopeeCushion,
+        Vuvuzela,
+        Ocarina,
+        Bagpipe,
+        Banjo,
+        Keytar,
+        GoldenRecorder,
+        RockOck,
+        Glockenspiel,
+        Theremin,
+        DidgeridooOfDestruction
     }
 }
