efd152d264084d600c92df0b65378f53d2656d39
cbrod <broderickcel@seattleu.edu>
2020-02-13 12:53:50 -0800

    Renamed converter class to ItemListToStringConverter for clarity

--- Game/Game/Helpers/ItemListToStringConverter.cs
+++ Game/Game/Helpers/ItemListToStringConverter.cs
@@ -7,7 +7,7 @@ using Xamarin.Forms;
 
 namespace Game.Helpers
 {
-    class ListToStringConverter : IValueConverter
+    class ItemListToStringConverter : IValueConverter
     {
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
