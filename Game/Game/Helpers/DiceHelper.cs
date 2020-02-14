74e41615c1b48b55df4c38bb6e5130160479128f
Alex Peterson <petersonalex@seattleu.edu>
2020-02-13 21:14:22 -0800

    Add comments in DiceHelper class.

--- Game/Game/Helpers/DiceHelper.cs
+++ Game/Game/Helpers/DiceHelper.cs
@@ -4,11 +4,17 @@ using System.Text;
 
 namespace Game.Helpers
 {
+    
+    /// <summary>
+    /// Dice helper class. Can simulate dice roll for 
+    /// values between "low" and "high"
+    /// </summary>
     class DiceHelper
     {
         Random r = new Random();
 
-        public int diceRoller(int low, int high)
+        // Get random dice roll 
+        public int DiceRoller(int low, int high)
         {
             var roll = r.Next(low, high);
             return roll;
