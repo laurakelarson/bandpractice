e181fac4a9bdcd7a3fb2ad29e665615ddba112ac
Alex Peterson <petersonalex@seattleu.edu>
2020-02-13 21:18:15 -0800

    Add comments to HomeMenuItemModel.

--- Game/Game/Models/HomeMenuItemModel.cs
+++ Game/Game/Models/HomeMenuItemModel.cs
@@ -5,8 +5,10 @@
     /// </summary>
     public class HomeMenuItemModel
     {
+        // Id of menu item 
         public MenuItemEnum Id { get; set; }
 
+        // Display string for menu item 
         public string Title { get; set; }
     }
 }
