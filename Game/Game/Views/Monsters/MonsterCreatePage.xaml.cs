904600f3b2b7028828c6d8c2343d9dfba36d3fa9
cbrod <broderickcel@seattleu.edu>
2020-02-13 17:37:04 -0800

    MonsterCreatePage: added health

--- Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
+++ Game/Game/Views/Monsters/MonsterCreatePage.xaml.cs
@@ -57,6 +57,16 @@ namespace Game.Views
         }
 
         /// <summary>
+        /// Catch the change to the Stepper for Health
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        void Health_OnStepperValueChanged(object sender, ValueChangedEventArgs e)
+        {
+            HealthValue.Text = String.Format("{0}", e.NewValue);
+        }
+
+        /// <summary>
         /// Catch the change to the Stepper for Range
         /// </summary>
         /// <param name="sender"></param>
