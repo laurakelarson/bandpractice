6b57587b88a0f454f30f9dedfb1c56e9a157bf80
Laura Larson <larsonlaura@seattleu.edu>
2020-02-16 20:04:28 -0800

    Added EntityService to use for both characters and monsters default images

---
+++ Game/Game/Services/EntityService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Services
+{
+    class EntityService
+    {
+        // Return the Default Image URI for the Local Image for a Character.
+        public static string DefaultCharacterImageURI = "default_icon.png";
+
+        // Return the Default Image URI for the Local Image for a Monster.
+        public static string DefaultMonsterImageURI = "monster_airhorn.png";
+    }
+}
