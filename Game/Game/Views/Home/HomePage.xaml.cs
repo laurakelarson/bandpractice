642ada5661ea6dfafb14d23e4626099ac43e3929
Laura Larson <larsonlaura@seattleu.edu>
2020-02-16 20:50:37 -0800

    Added comments to Hompage.xaml.cs

--- Game/Game/Views/Home/HomePage.xaml.cs
+++ Game/Game/Views/Home/HomePage.xaml.cs
@@ -31,12 +31,21 @@ namespace Game.Views
 			await Navigation.PushAsync(new MyBandPage()); 
 		}
 
-
+		/// <summary>
+		/// Button click method to open the Encyclopedia page which goes to CRUDi pages for each category
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		public async void Encyclopedia_Button_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushAsync(new EncyclopediaPage());
 		}
 
+		/// <summary>
+		/// Button click method to open High Score modal page to display highest achieved score
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
 		public async void HighScore_Button_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PushModalAsync(new NavigationPage(new HighScorePage(new GenericViewModel<ScoreModel>())));
