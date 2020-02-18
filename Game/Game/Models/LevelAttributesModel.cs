c1baaf35d2919433b428a17f4b315da44d72d269
Alex Peterson <petersonalex@seattleu.edu>
2020-02-17 18:15:57 -0800

    Add fields to LevelAttributesModel.

--- Game/Game/Models/LevelAttributesModel.cs
+++ Game/Game/Models/LevelAttributesModel.cs
@@ -6,6 +6,21 @@ namespace Game.Models
 {
     public class LevelAttributesModel
     {
+        // The level
+        public int Level;
+
+        // Base experience for this level
+        public int Experience;
+
+        // Attack bonus 
+        public int Attack;
+
+        // Defense bonus
+        public int Defense;
+
+        // Speed bonus 
+        public int Speed; 
+
 
     }
 }
