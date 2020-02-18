7229bc402ee9f1b24045daf4b3da3c92fb45fb77
Alex Peterson <petersonalex@seattleu.edu>
2020-02-17 18:27:51 -0800

    Add LevelAttributesList to LevelAttributesHelper.

--- Game/Game/Helpers/LevelAttributesHelper.cs
+++ Game/Game/Helpers/LevelAttributesHelper.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Game.Models;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -29,5 +30,7 @@ namespace Game.Helpers
 
         #endregion Singleton
 
+        // List of all levels and attributes 
+        public List<LevelAttributesModel> LevelAttributesList { get; set; }
     }
 }
