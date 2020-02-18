12bab8cb8d02358fdea23c5fc381c314a2c6f913
cbrod <broderickcel@seattleu.edu>
2020-02-18 14:21:41 -0800

    DataSetsHelper: added WarmUp method

--- Game/Game/Helpers/DataSetsHelper.cs
+++ Game/Game/Helpers/DataSetsHelper.cs
@@ -1,10 +1,22 @@
 ﻿using System;
+using Game.ViewModels;
+
 namespace Game.Helpers
 {
-    public class DataSetsHelper
+    public static class DataSetsHelper
     {
-        public DataSetsHelper()
+        /// <summary>
+        /// Warms up and loads the data sets in the correct order to account for data dependencies
+        /// </summary>
+        public static void WarmUp()
         {
+            // load items before monster and character
+            ItemIndexViewModel.Instance.GetCurrentDataSource();
+
+            ScoreIndexViewModel.Instance.GetCurrentDataSource();
+
+            CharacterIndexViewModel.Instance.GetCurrentDataSource();
+            MonsterIndexViewModel.Instance.GetCurrentDataSource();
         }
     }
 }
