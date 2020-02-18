27e6e1b5455d363cacc26c6ae7dccc7d876d579a
Alex Peterson <petersonalex@seattleu.edu>
2020-02-17 18:19:17 -0800

    Add constructor and class comments.

--- Game/Game/Models/LevelAttributesModel.cs
+++ Game/Game/Models/LevelAttributesModel.cs
@@ -4,6 +4,9 @@ using System.Text;
 
 namespace Game.Models
 {
+    /** LevelAttributesModel class. This class exists to make it 
+     * easier to adjust the attributes of characters when they 
+     * level up. **/
     public class LevelAttributesModel
     {
         // The level
@@ -21,6 +24,23 @@ namespace Game.Models
         // Speed bonus 
         public int Speed; 
 
-
+        /// <summary>
+        /// Constructor. Creates level with passed in 
+        /// attributes. 
+        /// </summary>
+        /// <param name="level"></param>
+        /// <param name="experience"></param>
+        /// <param name="attack"></param>
+        /// <param name="defense"></param>
+        /// <param name="speed"></param>
+        public LevelAttributesModel(int level, int experience, int attack, 
+            int defense, int speed)
+        {
+            Level = level;
+            Experience = experience;
+            Attack = attack;
+            Defense = defense;
+            Speed = speed; 
+        }
     }
 }
