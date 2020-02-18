7e9f74dc0c9c7180b441ab5741f125d59d9113e0
Alex Peterson <petersonalex@seattleu.edu>
2020-02-17 18:25:04 -0800

    Make LevelAttributesHelper Singleton.

--- Game/Game/Helpers/LevelAttributesHelper.cs
+++ Game/Game/Helpers/LevelAttributesHelper.cs
@@ -4,7 +4,30 @@ using System.Text;
 
 namespace Game.Helpers
 {
+    /// <summary>
+    /// Helper class that keeps a list of levels and attributes
+    /// to simplify leveling up characters. 
+    /// </summary>
     public class LevelAttributesHelper
     {
+
+        #region Singleton
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static LevelAttributesHelper instance;
+
+        public static LevelAttributesHelper Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    instance = new LevelAttributesHelper();
+                }
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
     }
 }
