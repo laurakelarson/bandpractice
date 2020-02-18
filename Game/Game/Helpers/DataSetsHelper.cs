db7dcbce1b904872a58f9092511828159d8d6def
cbrod <broderickcel@seattleu.edu>
2020-02-18 14:23:56 -0800

    DataSetsHelper: added comment to explain class

--- Game/Game/Helpers/DataSetsHelper.cs
+++ Game/Game/Helpers/DataSetsHelper.cs
@@ -3,6 +3,11 @@ using Game.ViewModels;
 
 namespace Game.Helpers
 {
+    /// <summary>
+    /// DataSetsHelper is a static class that helps manage data dependencies in the default data sets.
+    /// For example, Monster Model may hold foreign keys into Item Model, so Item Model needs to be loaded first.
+    /// That way, when Monster Model data is loaded, the model is able to locate the associated Items.
+    /// </summary>
     public static class DataSetsHelper
     {
         /// <summary>
