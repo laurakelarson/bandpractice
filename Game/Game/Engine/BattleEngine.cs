e53f4c7623250b6d889b6416cc974ae01241cc4b
cbrod <broderickcel@seattleu.edu>
2020-02-20 22:58:19 -0800

    BattleEngine: add comment and property for whether battle is running

--- Game/Game/Engine/BattleEngine.cs
+++ Game/Game/Engine/BattleEngine.cs
@@ -4,7 +4,14 @@ using System.Text;
 
 namespace Game.Engine
 {
+    /// <summary>
+    /// Battle Engine for the game.
+    /// Container for battle rounds (inheritance from RoundEngine) and
+    /// battle turns (RoundEngine inherits from TurnEngine)
+    /// </summary>
     class BattleEngine : RoundEngine
     {
+        // Track whether battle is running
+        public bool BattleRunning = false;
     }
 }
