677ef2f399e47c5baaccdec1b87fa15addf95e8c
cbrod <broderickcel@seattleu.edu>
2020-02-20 23:10:31 -0800

    RoundEngine: added dummy for NewRound method (in order to call from BattleEngine)

--- Game/Game/Engine/RoundEngine.cs
+++ Game/Game/Engine/RoundEngine.cs
@@ -6,5 +6,16 @@ namespace Game.Engine
 {
     class RoundEngine : TurnEngine
     {
+
+        /// <summary>
+        /// Start a new round in the battle
+        /// </summary>
+        /// <returns></returns>
+        public bool NewRound()
+        {
+            //TODO implement
+            return true;
+        }
+
     }
 }
