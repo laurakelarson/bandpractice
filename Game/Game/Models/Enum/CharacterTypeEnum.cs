2b3f0aaa7f9e65a35253cb45ab8d404795065495
cbrod <broderickcel@seattleu.edu>
2020-02-20 20:56:48 -0800

    CharacterTypeEnum: add numeric values + Uknown type

--- Game/Game/Models/Enum/CharacterTypeEnum.cs
+++ Game/Game/Models/Enum/CharacterTypeEnum.cs
@@ -1,15 +1,20 @@
 ﻿namespace Game.Models
 {
     /// <summary>
-    /// Available Character types
+    /// Available Character types.
+    /// Used by Character CRUDi to help create characters of a certain type.
     /// </summary>
     public enum CharacterTypeEnum
     {
-        TambourinePlayer,
-        Bassist,
-        Keyboardist,
-        Drummer,
-        Guitarist,
-        LeadVocalist
+        // Not specified
+        Unknown = 0,
+
+        // All the character types available in game
+        TambourinePlayer = 1,
+        Bassist = 2,
+        Keyboardist = 3,
+        Drummer = 4,
+        Guitarist = 5,
+        LeadVocalist = 6
     }
 }
