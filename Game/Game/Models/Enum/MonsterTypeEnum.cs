c86e1eb0c41ea55083ff163e2495c2f7dda52fc3
cbrod <broderickcel@seattleu.edu>
2020-02-20 21:09:43 -0800

    MonsterTypeEnum: add 'Unknown' and numerical values

--- Game/Game/Models/Enum/MonsterTypeEnum.cs
+++ Game/Game/Models/Enum/MonsterTypeEnum.cs
@@ -2,31 +2,36 @@
 {
     /// <summary>
     /// Available monster types
+    /// Used by DefaultData helper to help create the default monsters.
     /// </summary>
     public enum MonsterTypeEnum
     {
-        Chomper,
-        MassiveStatic,
-        Motobeast,
-        Kazoom,
-        Panpot,
-        Jackhammer,
-        Brakez,
-        Driller,
-        Alarmer,
-        ShrillBabe,
-        BuzzRowdy,
-        PiercingFeedback,
-        FranDrescher,
-        YowlingFeline,
-        Nickelback,
-        LloydChristmas,
-        RecorderApprentice,
-        AirhornLeviathan,
-        EarsplittingChalkboard,
-        RubberChickenBlob,
-        AgonizingSilence,
-        GilbertGottfried
+        // Not specified
+        Unknown = 0,
+
+        // all the default monsters available in the game
+        Chomper = 1,
+        MassiveStatic = 2,
+        Motobeast = 3,
+        Kazoom = 4,
+        Panpot = 5,
+        Jackhammer = 6,
+        Brakez = 7,
+        Driller = 8,
+        Alarmer = 9,
+        ShrillBabe = 10,
+        BuzzRowdy = 11,
+        PiercingFeedback = 12,
+        FranDrescher = 13,
+        YowlingFeline = 14,
+        Nickelback = 15,
+        LloydChristmas = 16,
+        RecorderApprentice = 17,
+        AirhornLeviathan = 18 ,
+        EarsplittingChalkboard = 19,
+        RubberChickenBlob = 20,
+        AgonizingSilence = 21,
+        GilbertGottfried = 22
 
     }
 }
