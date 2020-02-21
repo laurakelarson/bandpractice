a3ffca18d052bd4e5cbd61bfebb76f67995eda21
cbrod <broderickcel@seattleu.edu>
2020-02-20 22:14:48 -0800

    BaseEngine: add properties

--- Game/Game/Engine/BaseEngine.cs
+++ Game/Game/Engine/BaseEngine.cs
@@ -1,10 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Models;
 
+/// <summary>
+/// Holds the data structures for the battle engine.
+/// </summary>
 namespace Game.Engine
 {
     class BaseEngine
     {
+        // The score of the battle
+        public ScoreModel Score = new ScoreModel();
+
+        // The pool of items dropped and collected throughout the battle round
+        public List<ItemModel> ItemPool = new List<ItemModel>();
+
+        // Monsters in the battle
+        public List<MonsterModel> MonsterList = new List<MonsterModel>();
+
+        // Characters in the battle
+        public List<CharacterModel> CharacterList = new List<CharacterModel>();
     }
 }
