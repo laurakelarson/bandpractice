4a30b83ad4bccb49cb59374998528f6c616a23f1
cbrod <broderickcel@seattleu.edu>
2020-02-20 21:09:18 -0800

    MenuItemEnum: add 'Unknown' and numerical values

--- Game/Game/Models/Enum/MenuItemEnum.cs
+++ Game/Game/Models/Enum/MenuItemEnum.cs
@@ -5,11 +5,25 @@
     /// </summary>
     public enum MenuItemEnum
     {
-        MyBand,
-        Home,
-        Encyclopedia,
-        Score,
-        Items,
-        About
+        // Not specified
+        Unknown = 0,
+
+        // MyBand page is the entry point to a battle
+        MyBand = 1,
+
+        // Home page
+        Home = 2,
+
+        // Encyclopedia page, landing point for CRUDi pages
+        Encyclopedia = 3,
+
+        // Score CRUDi
+        Score = 4,
+
+        // Items CRUDi
+        Items = 5,
+
+        // About page, includes debug settings
+        About = 6
     }
 }
