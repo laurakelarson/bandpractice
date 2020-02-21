bf7c8cc3cfbb80168af5b55a4c5554833bbe7b49
cbrod <broderickcel@seattleu.edu>
2020-02-20 21:08:57 -0800

    ItemTypeEnum: add 'Unknown' and numerical values

--- Game/Game/Models/Enum/ItemTypeEnum.cs
+++ Game/Game/Models/Enum/ItemTypeEnum.cs
@@ -7,39 +7,44 @@ namespace Game.Models.Enum
 
     /// <summary>
     /// Available Item types
+    /// Used by DefaultData to help create the default items in the game.
     /// </summary>
     public enum ItemTypeEnum
     {
-        Earplugs,
-        Earmuffs,
-        NoiseCancelingHeadphones,
-        Microphone,
-        Coffee,
-        EnergyDrink,
-        Metronome,
-        TuningFork,
-        BandTshirt,
-        BandHoodie,
-        CoolOutfit,
-        Ring,
-        MoodRing,
-        TemporaryTattoo,
-        AthleticSocks,
-        LuckySocks,
-        ComfySneakers,
-        BunnySlippers,
-        Triangle,
-        PrankDoorbell,
-        WhoopeeCushion,
-        Vuvuzela,
-        Ocarina,
-        Bagpipe,
-        Banjo,
-        Keytar,
-        GoldenRecorder,
-        RockOck,
-        Glockenspiel,
-        Theremin,
-        DidgeridooOfDestruction
+        // Not specified
+        Unknown = 0,
+
+        // All the default items available in game
+        Earplugs = 1,
+        Earmuffs = 2,
+        NoiseCancelingHeadphones = 3,
+        Microphone = 4,
+        Coffee = 5,
+        EnergyDrink = 6,
+        Metronome = 7,
+        TuningFork = 8,
+        BandTshirt = 9,
+        BandHoodie = 10,
+        CoolOutfit = 11,
+        Ring = 12,
+        MoodRing = 13,
+        TemporaryTattoo = 14,
+        AthleticSocks = 15,
+        LuckySocks = 16,
+        ComfySneakers = 17,
+        BunnySlippers = 18,
+        Triangle = 19,
+        PrankDoorbell = 20,
+        WhoopeeCushion = 21,
+        Vuvuzela = 22,
+        Ocarina = 23,
+        Bagpipe = 24,
+        Banjo = 25,
+        Keytar = 26,
+        GoldenRecorder = 27,
+        RockOck = 28,
+        Glockenspiel = 29,
+        Theremin = 30,
+        DidgeridooOfDestruction = 31
     }
 }
