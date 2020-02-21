c9621a6ec4c996783fe629687c3d37ad920cc6c5
cbrod <broderickcel@seattleu.edu>
2020-02-20 22:51:32 -0800

    Add file for BattleMessagesModel

---
+++ Game/Game/Models/BattleMessagesModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace Game.Models
+{
+    /// <summary>
+    /// Manages the Message formatting for the UI to Display
+    /// </summary>
+    public class BattleMessagesModel
+    {
+        public BattleMessagesModel()
+        {
+        }
+    }
+}
