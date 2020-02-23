1c59de98e7324545e2e945f9630e07e1b8ef81f6
Laura Larson <larsonlaura@seattleu.edu>
2020-02-23 15:47:31 -0800

    Added Range attributes to LevelAttributesHelper

--- Game/Game/Helpers/LevelAttributesHelper.cs
+++ Game/Game/Helpers/LevelAttributesHelper.cs
@@ -57,29 +57,29 @@ namespace Game.Helpers
         {
             // Use level zero for zero index of array, so 
             // array index is same as level 
-            LevelAttributesList.Add(new LevelAttributesModel(0, 0, 0, 0, 0));
+            LevelAttributesList.Add(new LevelAttributesModel(0, 0, 0, 0, 0, 0));
 
             // Character level table 
-            LevelAttributesList.Add(new LevelAttributesModel(1, 0, 1, 1, 1));
-            LevelAttributesList.Add(new LevelAttributesModel(2, 300, 1, 2, 1));
-            LevelAttributesList.Add(new LevelAttributesModel(3, 900, 2, 3, 1));
-            LevelAttributesList.Add(new LevelAttributesModel(4, 2700, 2, 3, 1));
-            LevelAttributesList.Add(new LevelAttributesModel(5, 6500, 2, 4, 2));
-            LevelAttributesList.Add(new LevelAttributesModel(6, 14000, 3, 4, 2));
-            LevelAttributesList.Add(new LevelAttributesModel(7, 23000, 3, 5, 2));
-            LevelAttributesList.Add(new LevelAttributesModel(8, 34000, 3, 5, 2));
-            LevelAttributesList.Add(new LevelAttributesModel(9, 48000, 3, 5, 2));
-            LevelAttributesList.Add(new LevelAttributesModel(10, 64000, 4, 6, 3));
-            LevelAttributesList.Add(new LevelAttributesModel(11, 85000, 4, 6, 3));
-            LevelAttributesList.Add(new LevelAttributesModel(12, 100000, 4, 6, 3));
-            LevelAttributesList.Add(new LevelAttributesModel(13, 120000, 4, 7, 3));
-            LevelAttributesList.Add(new LevelAttributesModel(14, 140000, 5, 7, 3));
-            LevelAttributesList.Add(new LevelAttributesModel(15, 165000, 5, 7, 4));
-            LevelAttributesList.Add(new LevelAttributesModel(16, 195000, 5, 8, 4));
-            LevelAttributesList.Add(new LevelAttributesModel(17, 225000, 5, 8, 4));
-            LevelAttributesList.Add(new LevelAttributesModel(18, 265000, 6, 8, 4));
-            LevelAttributesList.Add(new LevelAttributesModel(19, 305000, 7, 9, 4));
-            LevelAttributesList.Add(new LevelAttributesModel(20, 355000, 8, 10, 5));
+            LevelAttributesList.Add(new LevelAttributesModel(1, 0, 1, 1, 1, 1));
+            LevelAttributesList.Add(new LevelAttributesModel(2, 300, 1, 2, 1, 1));
+            LevelAttributesList.Add(new LevelAttributesModel(3, 900, 2, 3, 1, 1));
+            LevelAttributesList.Add(new LevelAttributesModel(4, 2700, 2, 3, 1, 2));
+            LevelAttributesList.Add(new LevelAttributesModel(5, 6500, 2, 4, 2, 2));
+            LevelAttributesList.Add(new LevelAttributesModel(6, 14000, 3, 4, 2, 2));
+            LevelAttributesList.Add(new LevelAttributesModel(7, 23000, 3, 5, 2, 3));
+            LevelAttributesList.Add(new LevelAttributesModel(8, 34000, 3, 5, 2, 3));
+            LevelAttributesList.Add(new LevelAttributesModel(9, 48000, 3, 5, 2, 3));
+            LevelAttributesList.Add(new LevelAttributesModel(10, 64000, 4, 6, 3, 3));
+            LevelAttributesList.Add(new LevelAttributesModel(11, 85000, 4, 6, 3, 4));
+            LevelAttributesList.Add(new LevelAttributesModel(12, 100000, 4, 6, 3, 4));
+            LevelAttributesList.Add(new LevelAttributesModel(13, 120000, 4, 7, 3, 4));
+            LevelAttributesList.Add(new LevelAttributesModel(14, 140000, 5, 7, 3, 4));
+            LevelAttributesList.Add(new LevelAttributesModel(15, 165000, 5, 7, 4, 5));
+            LevelAttributesList.Add(new LevelAttributesModel(16, 195000, 5, 8, 4, 5));
+            LevelAttributesList.Add(new LevelAttributesModel(17, 225000, 5, 8, 4, 5));
+            LevelAttributesList.Add(new LevelAttributesModel(18, 265000, 6, 8, 4, 5));
+            LevelAttributesList.Add(new LevelAttributesModel(19, 305000, 7, 9, 4, 6));
+            LevelAttributesList.Add(new LevelAttributesModel(20, 355000, 8, 10, 5, 6));
 
         }
     }
