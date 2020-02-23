2a0e88fca4b5cea44601fbec7b1fe062697324a8
cbrod <broderickcel@seattleu.edu>
2020-02-23 15:57:48 -0800

    RoundEngine: add ClearLists method

--- Game/Game/Engine/RoundEngine.cs
+++ Game/Game/Engine/RoundEngine.cs
@@ -10,6 +10,17 @@ namespace Game.Engine
     /// </summary>
     public class RoundEngine : TurnEngine
     {
+        /// <summary>
+        /// Clear the monster and item lists between Rounds
+        /// </summary>
+        /// <returns></returns>
+        private bool ClearLists()
+        {
+            ItemPool.Clear();
+            MonsterList.Clear();
+            return true;
+        }
+
 
         /// <summary>
         /// Start a new round in the battle
