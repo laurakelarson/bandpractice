b0be4001215e4bcf20294a0c168e77227478d9db
cbrod <broderickcel@seattleu.edu>
2020-02-23 14:54:54 -0800

    BattleEngineTests: add test for constructor

--- UnitTests/Engine/BattleEngineTests.cs
+++ UnitTests/Engine/BattleEngineTests.cs
@@ -28,11 +28,17 @@ namespace UnitTests.Engine
         }
 
         [Test]
-        public void TestMethod()
+        public void BattleEngine_Constructor_Default_Should_Pass()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            // Arrange
+
+            // Act
+            var result = Engine;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
