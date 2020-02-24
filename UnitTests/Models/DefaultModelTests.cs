e74ff121a3fe3403f08a43bd625babadd251b8df
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 12:43:03 -0800

    Add Test DefaultModel_Constructor_Should_Pass()

---
+++ UnitTests/Models/DefaultModelTests.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using Game.Models;
+
+namespace UnitTests.Models
+{
+    class DefaultModelTests
+    {
+        [Test]
+        public void DefaultModel_Default_Constructor_Should_Pass()
+        {
+            // Arrange
+            var data = new DefaultModel();
+            // Act
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(data);
+        }
+
+        [Test]
+        public void DefaultModel_Constructor_Should_Pass()
+        {
+            // Arrange
+            var data = new DefaultModel
+            {
+                Name = "name",
+                Description = "desc",
+                Id = "id"
+            };
+            
+            // Act
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual("name", data.Name);
+            Assert.AreEqual("desc", data.Description);
+            Assert.AreEqual("id", data.Id);
+        }
+    }
+}
