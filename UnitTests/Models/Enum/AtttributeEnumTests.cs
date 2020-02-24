3c125b009bb74c5c861d47bfd449c6cf621fcd17
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 16:24:15 -0800

    Add AttributeEnum test for Speed.

--- UnitTests/Models/Enum/AtttributeEnumTests.cs
+++ UnitTests/Models/Enum/AtttributeEnumTests.cs
@@ -15,5 +15,12 @@ namespace UnitTests.Models.Enum
             var result = (int)AttributeEnum.Unknown; 
             Assert.AreEqual(0, result); 
         }
+
+        [Test]
+        public void AttributeEnumTests_Default_Speed_Should_Pass()
+        {
+            var result = (int)AttributeEnum.Speed;
+            Assert.AreEqual(10, result);
+        }
     }
 }
