80c176fb9509429b3e55a049698072cc39428e4f
Laura Larson <larsonlaura@seattleu.edu>
2020-02-23 23:36:43 -0800

    Added ScoreModel constructor default should pass test to ScoreModel UT

--- UnitTests/Models/ScoreModelTests.cs
+++ UnitTests/Models/ScoreModelTests.cs
@@ -9,6 +9,19 @@ namespace UnitTests.Models
     [TestFixture]
     public class ScoreModelTests
     {
-       
+        [Test]
+        public void ScoreModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ScoreModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
+
     }
 }
