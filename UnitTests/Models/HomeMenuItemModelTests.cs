563f17a012ea7cdfc08d2b96c8fd56130e072964
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 01:06:49 -0800

    Added HomeMenuItemModel set default should pass to UT

--- UnitTests/Models/HomeMenuItemModelTests.cs
+++ UnitTests/Models/HomeMenuItemModelTests.cs
@@ -22,5 +22,26 @@ namespace UnitTests.Models
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void HomeMenuItemModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+            var result = new HomeMenuItemModel();
+
+            // Act
+
+            // Test all the Setters
+            result.Id = MenuItemEnum.Encyclopedia;
+            result.Title = "bogus title";
+
+            // Reset
+
+            // Assert
+
+            // The Get is tested by retrieving it here as well.
+            Assert.AreEqual("bogus title", result.Title);
+            Assert.AreEqual(MenuItemEnum.Encyclopedia, result.Id);
+        }
     }
 }
