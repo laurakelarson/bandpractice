1376b91b95f878f85eec503c624f7edafef9de91
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 19:23:32 -0800

    Add MenuItemEnum test for default values.

--- UnitTests/Models/Enum/MenuItemEnumTests.cs
+++ UnitTests/Models/Enum/MenuItemEnumTests.cs
@@ -1,10 +1,41 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class MenuItemEnumTests
+    /// <summary>
+    ///  AttributeEnum tests. 
+    /// </summary>
+    [TestFixture]
+    public class MenuItemEnumTests
     {
+        [Test]
+        public void MenuItemEnumTests_Default_Values_Should_Pass()
+        {
+            // Arrange 
+
+            // Act 
+            var unknown = (int)MenuItemEnum.Unknown;
+            var myBand = (int)MenuItemEnum.MyBand;
+            var home = (int)MenuItemEnum.Home;
+            var encyclopedia = (int)MenuItemEnum.Encyclopedia;
+            var score = (int)MenuItemEnum.Score;
+            var items = (int)MenuItemEnum.Items;
+            var about = (int)MenuItemEnum.About;
+
+            // Reset 
+
+            // Assert
+            Assert.AreEqual(0, unknown);
+            Assert.AreEqual(1, myBand);
+            Assert.AreEqual(2, home);
+            Assert.AreEqual(3, encyclopedia);
+            Assert.AreEqual(4, score);
+            Assert.AreEqual(5, items);
+            Assert.AreEqual(6, about); 
+        }
     }
 }
