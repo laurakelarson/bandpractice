621819388665d2b139df200efb5eae8427fb5998
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 12:31:28 -0800

    Add test LevelAttributesModel_Constructor_Should_Pass()

--- UnitTests/Models/LevelAttributesModelTests.cs
+++ UnitTests/Models/LevelAttributesModelTests.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Models;
 using NUnit.Framework;
 
 namespace UnitTests.Models
@@ -8,5 +9,18 @@ namespace UnitTests.Models
     [TestFixture]
     class LevelAttributesModelTests
     {
+        [Test]
+        public void LevelAttributesModel_Constructor_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new LevelAttributesModel(1, 1, 1, 1, 1, 1);
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
