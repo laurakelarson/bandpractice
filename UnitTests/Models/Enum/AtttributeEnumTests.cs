4692a76e6f2f079176d4a40b26c58196a8c3da01
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 16:22:59 -0800

    Add AttributeEnum test for 'Unknown'.

--- UnitTests/Models/Enum/AtttributeEnumTests.cs
+++ UnitTests/Models/Enum/AtttributeEnumTests.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class AtttributeEnumTests
+    [TestFixture]
+    public class AtttributeEnumTests
     {
+        [Test]
+        public void AttributeEnumTests_Default_Unknown_Should_Pass()
+        {
+            var result = (int)AttributeEnum.Unknown; 
+            Assert.AreEqual(0, result); 
+        }
     }
 }
