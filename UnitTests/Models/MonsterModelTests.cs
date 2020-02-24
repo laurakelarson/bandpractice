f324ea338abeafaf7ccecf617f461bdaf7952466
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 10:22:31 -0800

    Added Monster set default should pass to UT

--- UnitTests/Models/MonsterModelTests.cs
+++ UnitTests/Models/MonsterModelTests.cs
@@ -56,5 +56,28 @@ namespace UnitTests.Models
             Assert.IsNotNull(result.Defense);
             Assert.IsNotNull(result.Level);
         }
+
+        [Test]
+        public void MonsterModel_Set_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new MonsterModel();
+            result.ExperienceGiven = 600;
+            result.Range = 7;
+            result.Boss = false;
+            result.ItemsDropped = "items";
+            result.UniqueDrops = "unique";
+
+            // Reset
+
+            // Assert 
+            Assert.AreEqual(600, result.ExperienceGiven);
+            Assert.AreEqual(7, result.Range);
+            Assert.AreEqual(false, result.Boss);
+            Assert.AreEqual("items", result.ItemsDropped);
+            Assert.AreEqual("unique", result.UniqueDrops);
+        }
     }
 }
