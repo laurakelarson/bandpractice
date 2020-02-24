3518a29d963386e29d38e1f9d5ae9457c10e4841
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 18:28:11 -0800

    Add HitStatusEnum test for Unknown.

--- UnitTests/Models/Enum/HitStatusEnumTests.cs
+++ UnitTests/Models/Enum/HitStatusEnumTests.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class HitStatusEnumTests
+    /// <summary>
+    ///  HitStatusEnum tests. 
+    /// </summary>
+    [TestFixture]
+    public class HitStatusEnumTests
     {
+        // Confirm Unknown set to correct value
+        [Test]
+        public void ItemLocationEnumTests_Default_Unknown_Should_Pass()
+        {
+            // Arrange 
+
+            // Act
+            var result = (int)HitStatusEnum.Unknown;
+
+            // Reset 
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
