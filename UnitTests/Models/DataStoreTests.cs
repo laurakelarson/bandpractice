f0983a7795f8e93d82fbfc4ee5b49e34f2f2f05f
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 01:11:57 -0800

    Added DataStore default unknown should pass to UT

--- UnitTests/Models/DataStoreTests.cs
+++ UnitTests/Models/DataStoreTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Models;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class DataStoreTests
     {
+        [Test]
+        public void DataStoreTests_Default_Unknown_Should_Pass()
+        {
+            // Arrange 
+
+            // Act 
+            var result = (int)DataStoreEnum.Unknown;
+
+            // Reset 
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
