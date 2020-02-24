9770d0b774bacf7325a678cf9c641c0bfc6a6959
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 00:24:03 -0800

    Added item model constructor new item should copy to item model UT

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -24,5 +24,22 @@ namespace UnitTests.Models
             // Assert 
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void ItemModel_Constructor_New_Item_Should_Copy()
+        {
+            // Arrange
+            var dataNew = new ItemModel();
+            dataNew.Value = 2;
+            dataNew.Id = "oldID";
+
+            // Act
+            var result = new ItemModel(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreNotEqual("oldID", result.Id);
+        }
     }
 }
