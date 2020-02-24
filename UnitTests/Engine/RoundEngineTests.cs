0c2f4d03b49ad07d785719cf4535dc8a4125ab0d
cbrod <broderickcel@seattleu.edu>
2020-02-23 21:10:38 -0800

    Add test RoundEngine_OrderEntityListByTurnOrder_Speed_Higher_Should_Pass()

--- UnitTests/Engine/RoundEngineTests.cs
+++ UnitTests/Engine/RoundEngineTests.cs
@@ -1,4 +1,5 @@
 ﻿using Game.Engine;
+using Game.Models;
 using NUnit.Framework;
 using System;
 using System.Collections;
@@ -45,5 +46,51 @@ namespace UnitTests.Engine
             // Assert
             Assert.IsNotNull(result);
         }
+
+        // test that EntityList order is correct
+        [Test]
+        public void RoundEngine_OrderEntityListByTurnOrder_Speed_Higher_Should_Pass()
+        {
+            // Arrange
+            var Monster = new MonsterModel
+            {
+                Speed = 20,
+                Level = 20,
+                CurrentHealth = 100,
+                ExperienceGiven = 1000,
+                Name = "Z",
+                Id = "me"
+            };
+
+            Engine.MonsterList.Clear();
+            Engine.MonsterList.Add(Monster);
+
+            var Character = new CharacterModel
+            {
+                Speed = 1,
+                Level = 1,
+                CurrentHealth = 2,
+                TotalExperience = 1,
+                Name = "C",
+            };
+
+            Engine.CharacterList.Clear();
+            Engine.CharacterList.Add(Character);
+
+            // Make the List
+            Engine.EntityList = Engine.MakeEntityList();
+
+            // Sort the list by Current Health, so it has to be resorted.
+            Engine.EntityList = Engine.EntityList.OrderBy(m => m.CurrentHealth).ToList();
+
+            // Act
+            var result = Engine.OrderEntityListByTurnOrder();
+
+            // Resets
+
+            // Assert
+            Assert.AreEqual("me", result[0].Id);
+        }
+
     }
 }
