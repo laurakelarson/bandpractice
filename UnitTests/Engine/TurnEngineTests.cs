a4acbcfcd93e6c6fe2a0758919582ace47d7999a
cbrod <broderickcel@seattleu.edu>
2020-02-24 09:53:18 -0800

    TurnEngineTests: add setup

--- UnitTests/Engine/TurnEngineTests.cs
+++ UnitTests/Engine/TurnEngineTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Game.Engine;
+using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,9 +9,27 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Engine
 {
+    /// <summary>
+    /// Unit tests for TurnEngine class
+    /// </summary>
     [TestFixture]
     public class TurnEngineTests
     {
+        // BattleEngine to use
+        BattleEngine Engine;
+
+        [SetUp]
+        public void SetUp()
+        {
+            Engine = new BattleEngine();
+            Engine.StartBattle(false);  // refresh state
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
         [Test]
         public void TestMethod()
         {
