2383871ec67f21b443c437e9e18227452d12f6d0
Laura Larson <larsonlaura@seattleu.edu>
2020-02-23 23:37:28 -0800

    Added ScoreModel constructor new item should copy test to ScoreModel UT

--- UnitTests/Models/ScoreModelTests.cs
+++ UnitTests/Models/ScoreModelTests.cs
@@ -23,5 +23,35 @@ namespace UnitTests.Models
             Assert.IsNotNull(result);
         }
 
+        [Test]
+        public void ScoreModel_Constructor_New_Item_Should_Copy()
+        {
+            // Arrange
+            var dataNew = new ScoreModel();
+
+            dataNew.Id = "oldID";
+
+            dataNew.BattleNumber = 100;
+            dataNew.ScoreTotal = 200;
+            dataNew.GameDate = System.DateTime.MinValue;
+            dataNew.AutoBattle = true;
+            dataNew.TurnCount = 300;
+            dataNew.RoundCount = 400;
+            dataNew.MonsterSlainNumber = 500;
+            dataNew.ExperienceGainedTotal = 600;
+            dataNew.CharacterAtDeathList = "characters";
+            dataNew.MonstersKilledList = "monsters";
+            dataNew.ItemsDroppedList = "items";
+
+
+            // Act
+            var result = new ScoreModel(dataNew);
+
+            // Reset
+
+            // Assert 
+            Assert.AreNotEqual("oldID", result.Id);
+        }
+
     }
 }
