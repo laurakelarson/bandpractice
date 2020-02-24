3e8b68e7c602e2e842242c4fd85bacefeec07aa2
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 16:42:24 -0800

    Add comments to CharacterTypeEnumTests.

--- UnitTests/Models/Enum/CharacterTypeEnumTests.cs
+++ UnitTests/Models/Enum/CharacterTypeEnumTests.cs
@@ -6,13 +6,24 @@ using System.Text;
 
 namespace UnitTests.Models.Enum
 {
+    /// <summary>
+    ///  CharacterTypeEnum tests. 
+    /// </summary>
     [TestFixture]
     public class CharacterTypeEnumTests
     {
+        // Confirm Unknown set to correct value
         [Test]
         public void CharacterTypeEnumTests_Default_Unknown_Should_Pass()
         {
+            // Arrange 
+
+            // Act
             var result = (int)CharacterTypeEnum.Unknown;
+
+            // Reset 
+
+            // Assert
             Assert.AreEqual(0, result);
         }
     }
