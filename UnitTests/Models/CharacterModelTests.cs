27f7114c87130838de7baf5a9abb25429f5dd8ac
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 12:46:13 -0800

    Add test CharacterModel_Constructor_Default_Should_Pass()

--- UnitTests/Models/CharacterModelTests.cs
+++ UnitTests/Models/CharacterModelTests.cs
@@ -1,10 +1,27 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Helpers;
+using Game.Models;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class CharacterModelTests
     {
+        [Test]
+        public void CharacterModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new CharacterModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
