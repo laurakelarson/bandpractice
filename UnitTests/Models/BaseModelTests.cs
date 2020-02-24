cdcdd6669a7e318c520c49bdb65917e57b7dded7
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 20:01:11 -0800

    Add default constructor test to BaseModelTests.

--- UnitTests/Models/BaseModelTests.cs
+++ UnitTests/Models/BaseModelTests.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models
 {
-    class BaseModelTests
+    /// <summary>
+    /// Unit tests for BaseModel class. 
+    /// </summary>
+    [TestFixture]
+    public class BaseModelTests
     {
+        [Test]
+        public void BaseModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BaseModel<ItemModel>();
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual("Name", result.Name);
+            Assert.AreEqual("Description", result.Description); 
+        }
     }
 }
