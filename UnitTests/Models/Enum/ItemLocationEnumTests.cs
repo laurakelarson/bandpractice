681ea9bab094e78655691952270f07b74a09bb97
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 16:58:00 -0800

    Add ItemLocationEnum test for Unknown.

--- UnitTests/Models/Enum/ItemLocationEnumTests.cs
+++ UnitTests/Models/Enum/ItemLocationEnumTests.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class ItemLocationEnumTests
+    /// <summary>
+    ///  ItemLocationEnum tests. 
+    /// </summary>
+    [TestFixture]
+    public class ItemLocationEnumTests
     {
+        // Confirm Unknown set to correct value
+        [Test]
+        public void ItemLocationEnumTests_Default_Unknown_Should_Pass()
+        {
+            // Arrange 
+
+            // Act
+            var result = (int)ItemLocationEnum.Unknown;
+
+            // Reset 
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
