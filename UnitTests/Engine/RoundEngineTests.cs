53f0d326daa8f458041199ad0caf4d4c673eaa2b
cbrod <broderickcel@seattleu.edu>
2020-02-23 20:47:29 -0800

    RoundEngineTests: add setup

--- UnitTests/Engine/RoundEngineTests.cs
+++ UnitTests/Engine/RoundEngineTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Game.Engine;
+using NUnit.Framework;
 using System;
 using System.Collections;
 using System.Collections.Generic;
@@ -8,9 +9,28 @@ using System.Threading.Tasks;
 
 namespace UnitTests.Engine
 {
+    /// <summary>
+    /// Unit tests for the RoundEngine class
+    /// </summary>
     [TestFixture]
     public class RoundEngineTests
     {
+        // BattleEngine for testing
+        BattleEngine Engine;
+
+        // Create a new BattleEngine and StartBattle
+        [SetUp]
+        public void Setup()
+        {
+            Engine = new BattleEngine();
+            Engine.StartBattle(false); // trigger new round
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
+
         [Test]
         public void TestMethod()
         {
