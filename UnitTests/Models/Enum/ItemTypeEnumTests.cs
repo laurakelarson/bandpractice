937ac09d62725fdcd7ff48d70f013da4464c1248
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 18:48:26 -0800

    Add ItemTypeEnum tests for Unknown.

--- UnitTests/Models/Enum/ItemTypeEnumTests.cs
+++ UnitTests/Models/Enum/ItemTypeEnumTests.cs
@@ -1,10 +1,30 @@
-﻿using System;
+﻿using Game.Models.Enum;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class ItemTypeEnumTests
+    /// <summary>
+    /// ItemTypeEnum unit tests
+    /// </summary>
+    [TestFixture]
+    public class ItemTypeEnumTests
     {
+        // Confirm Unknown set to correct value
+        [Test]
+        public void ItemTypeEnumTests_Default_Unknown_Should_Pass()
+        {
+            // Arrange 
+
+            // Act
+            var result = (int)ItemTypeEnum.Unknown;
+
+            // Reset 
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
