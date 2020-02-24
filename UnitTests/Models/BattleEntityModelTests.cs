df34f381b631449bfc4e60b0de181e0470e20d6c
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 20:29:10 -0800

    Add default constructor test to BattleEntityModel tests.

--- UnitTests/Models/BattleEntityModelTests.cs
+++ UnitTests/Models/BattleEntityModelTests.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using Game.Models;
+using Game.Models.Enum;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models
 {
-    class BattleEntityModelTests
+    /// <summary>
+    /// BattleEntityModel tests. 
+    /// </summary>
+    [TestFixture]
+    public class BattleEntityModelTests
     {
+        // Test default Constructor
+        public void BattleEntityModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BattleEntityModel();
+
+            // reset
+
+            // Assert
+            Assert.AreEqual(result.EntityType, EntityTypeEnum.Unknown);
+            Assert.AreEqual(result.ExperiencePoints, 0);
+            Assert.AreEqual(result.ListOrder, 0); 
+        }
     }
 }
