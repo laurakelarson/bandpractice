19c0ee119654ec09a7071bf1bb7fb3700296b581
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 01:06:06 -0800

    Added HomeMenuItemModel constructor default should pass to UT

--- UnitTests/Models/HomeMenuItemModelTests.cs
+++ UnitTests/Models/HomeMenuItemModelTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Models;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class HomeMenuItemModelTests
     {
+        [Test]
+        public void HomeMenuItemModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new HomeMenuItemModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
