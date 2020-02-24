6c2db730dc26104ad9ff589b389f0af7e3647789
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 01:18:50 -0800

    Added Monster constructor default should pass to UT

--- UnitTests/Models/MonsterModelTests.cs
+++ UnitTests/Models/MonsterModelTests.cs
@@ -1,10 +1,26 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Game.Models;
+using NUnit.Framework;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class MonsterModelTests
     {
+        [Test]
+        public void MonsterModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new MonsterModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
