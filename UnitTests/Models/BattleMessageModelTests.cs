ee030bb8aca6328068fd483953080aced6d22b9c
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 21:26:25 -0800

    Add Default constructor test to BattleMessageModelTests.

--- UnitTests/Models/BattleMessageModelTests.cs
+++ UnitTests/Models/BattleMessageModelTests.cs
@@ -1,10 +1,29 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models
 {
-    class BattleMessageModelTests
+    [TestFixture]
+    public class BattleMessageModelTests
     {
+        // Test Default constructor 
+        [Test]
+        public void BattleMessageModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new BattleMessagesModel();
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
     }
 }
