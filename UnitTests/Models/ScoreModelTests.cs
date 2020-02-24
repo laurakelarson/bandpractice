2c9b8db0c42d94a76e7f9da6fd4b5b564c47b39f
Laura Larson <larsonlaura@seattleu.edu>
2020-02-23 23:29:41 -0800

    Added ScoreModelTest class

---
+++ UnitTests/Models/ScoreModelTests.cs
@@ -0,0 +1,14 @@
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    public class ScoreModelTests
+    {
+       
+    }
+}
