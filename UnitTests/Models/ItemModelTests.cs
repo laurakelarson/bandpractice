eeb1e9d6e4e885fa4149b73a1c043896278eda05
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 00:23:02 -0800

    Added item model constructor default should pass to item model UT

--- UnitTests/Models/ItemModelTests.cs
+++ UnitTests/Models/ItemModelTests.cs
@@ -1,10 +1,28 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using NUnit.Framework;
+
+using Game.Models;
+using Game.Helpers;
 
 namespace UnitTests.Models
 {
+    [TestFixture]
     class ItemModelTests
     {
+        [Test]
+        public void ItemModel_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = new ItemModel();
+
+            // Reset
+
+            // Assert 
+            Assert.IsNotNull(result);
+        }
     }
 }
