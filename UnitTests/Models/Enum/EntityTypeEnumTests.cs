1d9ddee1692bd7a19e744095a2b5cf0bc3a031d1
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 18:16:59 -0800

    Add EntityTypeEnum test for Unknown.

--- UnitTests/Models/Enum/EntityTypeEnumTests.cs
+++ UnitTests/Models/Enum/EntityTypeEnumTests.cs
@@ -1,10 +1,27 @@
-﻿using System;
+﻿using Game.Models.Enum;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class EntityTypeEnumTests
+    [TestFixture]
+    public class EntityTypeEnumTests
     {
+        // Confirm Unknown set to correct value 
+        [Test]
+        public void EntityTypeEnumTests_Default_Unknown_Should_Pass()
+        {
+            // Arrange 
+
+            // Act 
+            var result = (int)EntityTypeEnum.Unknown;
+
+            // Reset 
+
+            // Assert
+            Assert.AreEqual(0, result);
+        }
     }
 }
