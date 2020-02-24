e18027c87aeec4d18baa5edbb57bad9739e7fc32
cbrod <broderickcel@seattleu.edu>
2020-02-23 17:01:45 -0800

    Add EntityTypeEnum to track the entity type in battle round

---
+++ Game/Game/Models/Enum/EntityTypeEnum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace Game.Models.Enum
+{
+    /// <summary>
+    /// Used to track the type of the entity in a battle.
+    /// </summary>
+    public enum EntityTypeEnum
+    {
+        // Not Known
+        Unknown = 0,
+
+        // Entity is a Character
+        Character = 1,
+
+        // Entity is a Monster
+        Monster = 2,
+    }
+}
