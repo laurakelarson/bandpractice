1e00496cd25cd3d3f9a4220178e956c8d8167435
Laura Larson <larsonlaura@seattleu.edu>
2020-02-24 00:12:36 -0800

    Added EntityModelTests to project file

---
+++ UnitTests/Models/EntityModelTests.cs
@@ -0,0 +1,15 @@
+﻿using NUnit.Framework;
+
+using Game.Models;
+using Game.ViewModels;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace UnitTests.Models
+{
+    [TestFixture]
+    class EntityModelTests
+    {
+        
+    }
+}
