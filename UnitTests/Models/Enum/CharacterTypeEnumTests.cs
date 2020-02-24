2ce705a3a78bdb09c106327c6eb69c51d4d1dfdb
Alex Peterson <petersonalex@seattleu.edu>
2020-02-23 16:31:33 -0800

    Add CharacterTypeEnum test for Unknown.

--- UnitTests/Models/Enum/CharacterTypeEnumTests.cs
+++ UnitTests/Models/Enum/CharacterTypeEnumTests.cs
@@ -1,10 +1,19 @@
-﻿using System;
+﻿using Game.Models;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace UnitTests.Models.Enum
 {
-    class CharacterTypeEnumTests
+    [TestFixture]
+    public class CharacterTypeEnumTests
     {
+        [Test]
+        public void CharacterTypeEnumTests_Default_Unknown_Should_Pass()
+        {
+            var result = (int)CharacterTypeEnum.Unknown;
+            Assert.AreEqual(0, result);
+        }
     }
 }
