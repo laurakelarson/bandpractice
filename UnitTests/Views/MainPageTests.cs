ce1f2641a8bca5e54cc3d69d8cc94cfbf9b38c65
Alex Peterson <petersonalex@seattleu.edu>
2020-02-24 20:28:05 -0800

    Add setup to MainPage tests.

--- UnitTests/Views/MainPageTests.cs
+++ UnitTests/Views/MainPageTests.cs
@@ -1,10 +1,31 @@
-﻿using System;
+﻿using Game;
+using Game.Views;
+using NUnit.Framework;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms; 
 
 namespace UnitTests.Views
 {
-    class MainPageTests
+    [TestFixture]
+    public class MainPageTests
     {
+        App app;
+        MainPage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new MainPage();
+        }
     }
 }
