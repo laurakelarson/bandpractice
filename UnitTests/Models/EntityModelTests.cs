22cb81be2ca63480856e4cbba0337b32c0e0cada
Alex Peterson <petersonalex@seattleu.edu>
2020-02-24 19:35:52 -0800

    Add test for EntityModel take damage method - entity takes damage but doesn't die.

--- UnitTests/Models/EntityModelTests.cs
+++ UnitTests/Models/EntityModelTests.cs
@@ -15,5 +15,23 @@ namespace UnitTests.Models
         {
             await Game.Helpers.DataSetsHelper.WipeData();
         }
+
+        // Test TakeDamage when character doesn't die from damage 
+        [Test]
+        public void EntityModelTests_TakeDamage_Dont_Die_Should_Pass()
+        {
+            // Arrange
+            var data = new CharacterModel();
+            data.CurrentHealth = 20;
+
+            // Act
+            data.TakeDamage(10);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(data.Alive, true);
+            Assert.AreEqual(data.CurrentHealth, 10); 
+        }
     }
 }
