b6a94f9e52bd86d4a7bfc347eaa0a6f93c7911a2
cbrod <broderickcel@seattleu.edu>
2020-03-01 18:33:56 -0800

    RecruitPage: add binding to BattleEngineViewModel

--- Game/Game/Views/Battle/RecruitPage.xaml.cs
+++ Game/Game/Views/Battle/RecruitPage.xaml.cs
@@ -1,15 +1,22 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Game.ViewModels;
 using Xamarin.Forms;
 
 namespace Game.Views.Battle
 {
     public partial class RecruitPage : ContentPage
     {
+        // Index View Model to help manage battle data across pages
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+
         public RecruitPage()
         {
             InitializeComponent();
+            Title = "My Band";
+
+            // Set up binding to BattleEngineViewModel
+            BindingContext = EngineViewModel;
         }
     }
 }
