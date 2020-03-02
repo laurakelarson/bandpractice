986608ae61003a292e4bbee52c825a3e2f65a46c
cbrod <broderickcel@seattleu.edu>
2020-03-01 22:31:57 -0800

    Add BandMemberPage to project

---
+++ Game/Game/Views/Battle/BandMemberPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Battle
+{
+    public partial class BandMemberPage : ContentPage
+    {
+        public BandMemberPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
