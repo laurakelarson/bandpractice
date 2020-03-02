fd56411ec5dca5f6cb236baff104541920d8dc91
cbrod <broderickcel@seattleu.edu>
2020-03-01 19:20:50 -0800

    CharacterConverterHelper: add comments

--- Game/Game/Helpers/CharacterConverterHelper.cs
+++ Game/Game/Helpers/CharacterConverterHelper.cs
@@ -4,14 +4,33 @@ using Xamarin.Forms;
 
 namespace Game.Helpers
 {
+    /// <summary>
+    /// This Converter is used help display a list of CharacterModel objects within a Picker.
+    /// It allows the picker to display the Character name and type.
+    /// </summary>
     public class CharacterConverter : IValueConverter
     {
-        
+        /// <summary>
+        /// Converts data type to visual representation. 
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
         }
 
+        /// <summary>
+        /// Converts visual representation to target data type.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <param name="targetType"></param>
+        /// <param name="parameter"></param>
+        /// <param name="culture"></param>
+        /// <returns></returns>
         public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
         {
             throw new NotImplementedException();
