0c5c1dd44ac87a43a3609218f05ab0bc374ba32c
cbrod <broderickcel@seattleu.edu>
2020-03-01 18:30:26 -0800

    Add RecruitPage to project

---
+++ Game/Game/Views/Battle/RecruitPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Game.Views.Battle
+{
+    public partial class RecruitPage : ContentPage
+    {
+        public RecruitPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
