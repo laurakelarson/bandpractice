6b306aa8a09b8a017c4923310fe51389a12db178
Laura Larson <larsonlaura@seattleu.edu>
2020-03-02 10:25:21 -0800

    Added GetCharacterName() to Random Entity Helper

--- Game/Game/Helpers/RandomEntityHelper.cs
+++ Game/Game/Helpers/RandomEntityHelper.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Game.Helpers
@@ -17,5 +18,30 @@ namespace Game.Helpers
             // Roll the Dice and reset the Health
             return DiceHelper.RollDice(level, 10);
         }
+
+        // get monster type
+
+        // get character name
+        /// <summary>
+        /// Get Name
+        /// 
+        /// Return a Random Name for a Character
+        /// </summary>
+        /// <returns></returns>
+        public static string GetCharacterName()
+        {
+
+            List<String> FirstNameList = new List<String> { "Frankie", "DJ Thunder", "Kit", "Vic", "Spark", "Ace", 
+                "Charlie", "Crazy Danni Chill", "Big Celeste Koopa", "Sovereign", "Avery", "Riley", "Dakota", "Reese", "Remi", "Mr. Laura", 
+                "Nicki Mythic", "Harley", "Dr. Funk", "Sage", "Dallas", "River", "Eden", "Angel Skye", "Sutton", "Ali", "MC Alex Duke",
+                "Lennox", "Quincy", "Ally A Trigga", "Monroe", "Devon", "Blade", "Francis", "Blair", "Memphis", "Bowie", 
+                "Rebel", "Ripley", "Indigo", "Zephyr", "Ari", "Sasha", "Jojo", "Mimi", "Maverick", "Rambo", "Jax", 
+                "T. Riddle", "Juniper", "DJ Rumor", "Lil Dizzee", "C. Dolla"};
+
+            var result = FirstNameList.ElementAt(DiceHelper.RollDice(1, FirstNameList.Count()) - 1);
+
+            return result;
+        }
+
     }
 }
