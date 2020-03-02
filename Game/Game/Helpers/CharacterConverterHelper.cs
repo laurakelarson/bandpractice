d6194db4347fe46d998955126c825ee5f8a3c8c7
cbrod <broderickcel@seattleu.edu>
2020-03-01 19:17:58 -0800

    Add CharacterConverterHelper to project file

---
+++ Game/Game/Helpers/CharacterConverterHelper.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using Xamarin.Forms;
+
+namespace Game.Helpers
+{
+    public class CharacterConverter : IValueConverter
+    {
+        
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
