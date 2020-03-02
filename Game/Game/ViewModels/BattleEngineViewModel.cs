b380114aa1403c19e6c2ec24f2f78a50828efb25
cbrod <broderickcel@seattleu.edu>
2020-03-01 17:51:00 -0800

    Adding BattleEngineViewModel to project

---
+++ Game/Game/ViewModels/BattleEngineViewModel.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.ObjectModel;
+using Game.Models;
+
+namespace Game.ViewModels
+{
+    /// <summary>
+    /// Index View Model
+    /// Manages the list of data records
+    /// </summary>
+    public class BattleEngineViewModel
+    {
+        #region Singleton
+
+        // Make this a singleton so it only exist one time because holds all the data records in memory
+        private static volatile BattleEngineViewModel instance;
+        private static readonly object syncRoot = new Object();
+
+        public static BattleEngineViewModel Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (syncRoot)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new BattleEngineViewModel();
+                        }
+                    }
+                }
+
+                return instance;
+            }
+        }
+
+        #endregion Singleton
+
+
+        /// <summary>
+        /// The Battle Engine
+        /// </summary>
+        public Engine.BattleEngine Engine = new Engine.BattleEngine();
+
+        // Hold the Proposed List of Characters for the Battle to Use
+        public ObservableCollection<CharacterModel> PartyCharacterList { get; set; } = new ObservableCollection<CharacterModel>();
+
+        // Hold the View Model to the CharacterIndexViewModel
+        public CharacterIndexViewModel DatabaseCharacterViewModel = CharacterIndexViewModel.Instance;
+
+        // Have the Database Character List point to the Character View Model List
+        public ObservableCollection<CharacterModel> DatabaseCharacterList { get; set; } = CharacterIndexViewModel.Instance.Dataset;
+
+        #region Constructor
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public BattleEngineViewModel()
+        {
+        }
+
+        #endregion Constructor
+    }
+}
