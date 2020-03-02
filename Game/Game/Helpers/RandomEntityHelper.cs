94ca380481e0b678f21d7ea90b59c95283f8c024
Laura Larson <larsonlaura@seattleu.edu>
2020-03-02 09:36:45 -0800

    Added GetHealth method to Random Entity Helper

--- Game/Game/Helpers/RandomEntityHelper.cs
+++ Game/Game/Helpers/RandomEntityHelper.cs
@@ -6,5 +6,16 @@ namespace Game.Helpers
 {
     class RandomEntityHelper
     {
+
+        /// <summary>
+        /// Get Health
+        /// </summary>
+        /// <param name="level"></param>
+        /// <returns></returns>
+        public static int GetHealth(int level)
+        {
+            // Roll the Dice and reset the Health
+            return DiceHelper.RollDice(level, 10);
+        }
     }
 }
