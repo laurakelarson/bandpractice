3f2f72f992133792bd86f310699ec8f0e3d17b1a
cbrod <broderickcel@seattleu.edu>
2020-03-01 22:45:46 -0800

    BandMemberPage: add view models to code behind

--- Game/Game/Views/Battle/BandMemberPage.xaml.cs
+++ Game/Game/Views/Battle/BandMemberPage.xaml.cs
@@ -1,15 +1,33 @@
 ﻿using System;
 using System.Collections.Generic;
-
+using Game.Models;
+using Game.ViewModels;
 using Xamarin.Forms;
 
 namespace Game.Views.Battle
 {
+    /// <summary>
+    /// Band Member page displays the character's details
+    /// </summary>
     public partial class BandMemberPage : ContentPage
     {
-        public BandMemberPage()
+        // View Model for item
+        readonly GenericViewModel<CharacterModel> ViewModel;
+
+        // Index View Model to help manage battle data across pages
+        public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+
+        /// <summary>
+        /// Constructor called with a view model
+        /// This is the primary way to open the page
+        /// The viewModel is the data that should be displayed
+        /// </summary>
+        public BandMemberPage(GenericViewModel<CharacterModel> data)
         {
             InitializeComponent();
+            Title = "Band Member";
+
+            BindingContext = this.ViewModel = data;
         }
     }
 }
