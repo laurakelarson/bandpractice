3ee73bad672e9c03b5ede6e5da8f34650c104dbf
Alex Peterson <petersonalex@seattleu.edu>
2020-03-03 09:17:45 -0800

    Implement INotifyPropertyChanged interface in beats model.

--- Game/Game/Models/BeatsModel.cs
+++ Game/Game/Models/BeatsModel.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Text;
 
 namespace Game.Models
@@ -9,11 +10,11 @@ namespace Game.Models
     /// the player has accumulated. Beats are used to recruit
     /// characters for battles. 
     /// </summary>
-    public class BeatsModel : BaseModel<BeatsModel>
+    public class BeatsModel : BaseModel<BeatsModel>, INotifyPropertyChanged
     {
         // The number of beats the player currently has. 
         public int Beats { get; set; } = 500000;
 
-
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 }
