773960a0cd1bd2b05eb8209c8939c141c534f1e3
Laura Larson <larsonlaura@seattleu.edu>
2020-03-03 00:47:25 -0800

    Added default positions for Monsters and Characters for grid to GridPositionHelper

--- Game/Game/Helpers/GridPositionHelper.cs
+++ Game/Game/Helpers/GridPositionHelper.cs
@@ -1,10 +1,19 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Text;
 
 namespace Game.Helpers
 {
-    class GridPositionHelper
+    public static class GridPositionHelper
     {
+        // Monster positions for battle grid starting spots, which don't change round to round
+        private static List<Point> MonsterPositions = new List<Point> { 
+            new Point(0,4), new Point(1,5), new Point(2,4), new Point(3,5), new Point(4,4), new Point(5,5)};
+
+        // Character positions for battle grid starting spots, which don't change round to round
+        private static List<Point> CharacterPositions = new List<Point> {
+            new Point(0,2), new Point(1,1), new Point(2,2), new Point(3,1), new Point(4,2), new Point(5,1)};
+
     }
 }
