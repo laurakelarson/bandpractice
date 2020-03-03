d7618264ef603dffd4837fb3eb76deb35c78cfae
Alex Peterson <petersonalex@seattleu.edu>
2020-03-03 09:15:14 -0800

    Add 'Beats' attribute to BeatsModel.

--- Game/Game/Models/BeatsModel.cs
+++ Game/Game/Models/BeatsModel.cs
@@ -4,7 +4,16 @@ using System.Text;
 
 namespace Game.Models
 {
-    class BeatsModel
+    /// <summary>
+    /// Beats model class. Stores the current number of beats
+    /// the player has accumulated. Beats are used to recruit
+    /// characters for battles. 
+    /// </summary>
+    public class BeatsModel : BaseModel<BeatsModel>
     {
+        // The number of beats the player currently has. 
+        public int Beats { get; set; } = 500000;
+
+
     }
 }
