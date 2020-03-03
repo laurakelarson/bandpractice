62ba125bedcb07a004c0585c0c97b2917f5db724
cbrod <broderickcel@seattleu.edu>
2020-03-02 21:50:41 -0800

    RoundOverPage: moved content from PickItemsPage onto RoundOverPage

--- Game/Game/Views/Battle/RoundOverPage.xaml.cs
+++ Game/Game/Views/Battle/RoundOverPage.xaml.cs
@@ -1,41 +1,43 @@
 ﻿using System;
+using Game.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Game.Views
 {
 	/// <summary>
-	/// The Main Game Page
+	/// The Round Over Page
+    /// Displays the items equipped from the item pool after the round is over.
 	/// </summary>
 	[XamlCompilation(XamlCompilationOptions.Compile)]
 	public partial class RoundOverPage: ContentPage
 	{
+		// Index View Model to help manage battle data across pages
+		public BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+
 		/// <summary>
 		/// Constructor
 		/// </summary>
 		public RoundOverPage()
 		{
-			InitializeComponent ();
+			InitializeComponent();
+
+			// Display the items equipped during the round
+			string DummyText = "Spot equipped bowtie.\nBowie equipped bunny slippers.\nKoopa equipped mushroom.\n"
+				+ "Dr. Dog equipped headphones.\nFall McCartney equipped didgeridoo.";
+			ItemsLabel.Text = DummyText;
+			//TODO hook up battle so real message is displayed
+			//ItemsLabel.Text = EngineViewModel.Engine.BattleMessages.GetItemsEquippedMessage();
 		}
 
 		/// <summary>
-		/// Start next Round, returning to the battle screen
+		/// Advance to the next round
 		/// </summary>
 		/// <param name="sender"></param>
 		/// <param name="e"></param>
-		async void NextButton_Clicked(object sender, EventArgs e)
+		async void NextRound_Clicked(object sender, EventArgs e)
 		{
 			await Navigation.PopModalAsync();
-		}
-
-		/// <summary>
-		/// Start next Round, returning to the battle screen
-		/// </summary>
-		/// <param name="sender"></param>
-		/// <param name="e"></param>
-		async void PickItems_Clicked(object sender, EventArgs e)
-		{
-			await Navigation.PushModalAsync(new PickItemsPage());
 		}
 	}
 }
