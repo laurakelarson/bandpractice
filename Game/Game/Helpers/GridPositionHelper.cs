b0230453de51bcb6db3ede99dc08ca57ed31045d
Laura Larson <larsonlaura@seattleu.edu>
2020-03-03 00:51:25 -0800

    Updated fields to be public in GridPositionHelper

--- Game/Game/Helpers/GridPositionHelper.cs
+++ Game/Game/Helpers/GridPositionHelper.cs
@@ -8,11 +8,11 @@ namespace Game.Helpers
     public static class GridPositionHelper
     {
         // Monster positions for battle grid starting spots, which don't change round to round
-        private static List<Point> MonsterPositions = new List<Point> { 
+        public static List<Point> MonsterPositions = new List<Point> { 
             new Point(0,4), new Point(1,5), new Point(2,4), new Point(3,5), new Point(4,4), new Point(5,5)};
 
         // Character positions for battle grid starting spots, which don't change round to round
-        private static List<Point> CharacterPositions = new List<Point> {
+        public static List<Point> CharacterPositions = new List<Point> {
             new Point(0,2), new Point(1,1), new Point(2,2), new Point(3,1), new Point(4,2), new Point(5,1)};
 
     }
