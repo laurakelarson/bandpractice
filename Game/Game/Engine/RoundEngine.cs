a8b98324ef22da47fcb2cdffbdd2574fccb2a03e
Laura Larson <larsonlaura@seattleu.edu>
2020-03-08 12:04:23 -0700

    Updated logic in Add Monsters to Round method

--- Game/Game/Engine/RoundEngine.cs
+++ Game/Game/Engine/RoundEngine.cs
@@ -95,9 +95,6 @@ namespace Game.Engine
             for (var i = 0; i < MaxNumberMonsters; i++)
             {
                 var data = monsters[i];
-                // slightly randomizes Monster levels
-                var level = range.ElementAt(DiceHelper.RollDice(1, range.Count()) - 1);
-                data.ChangeLevel(level);
 
                 // Update monster's grid position
                 data.RowPos = GridPositionHelper.MonsterPositions[i].X;
