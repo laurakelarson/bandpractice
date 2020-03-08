8bce3710567e1d89f90ac2e93b9a63ae85af4374
Laura Larson <larsonlaura@seattleu.edu>
2020-03-08 11:13:57 -0700

    Added test AutoBattleEngine_RunAutoBattle_Monsters_1_Should_Pass()

--- UnitTests/ScenarioTests/ScenarioTests.cs
+++ UnitTests/ScenarioTests/ScenarioTests.cs
@@ -40,5 +40,41 @@ namespace UnitTests.ScenarioTests
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public async Task AutoBattleEngine_RunAutoBattle_Monsters_1_Should_Pass()
+        {
+            //Arrange
+
+            // Add Characters
+
+            Engine.MaxNumberCharacters = 1;
+
+            var CharacterPlayerBogus = new CharacterModel(
+                            new CharacterModel
+                            {
+                                Speed = -1,
+                                Level = 10,
+                                CurrentHealth = 11,
+                                TotalExperience = 1,
+                                Name = "Bogus"
+                            });
+
+            Engine.CharacterList.Add(CharacterPlayerBogus);
+
+
+            // Add Monsters
+
+            // Need to set the Monster count to 1, so the battle goes to Next Round Faster
+            Engine.MaxNumberMonsters = 1;
+
+            //Act
+            var result = await Engine.RunAutoBattle();
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result);
+        }
     }
 }
