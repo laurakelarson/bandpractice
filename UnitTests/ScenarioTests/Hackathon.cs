2cca0d37c85f3d9be9fa21f33575ac73c4edefb5
Laura Larson <larsonlaura@seattleu.edu>
2020-03-09 10:46:22 -0700

    Added test HackathonScenario_Constructor_0_Default_Should_Pass()

--- UnitTests/ScenarioTests/Hackathon.cs
+++ UnitTests/ScenarioTests/Hackathon.cs
@@ -28,5 +28,89 @@ namespace UnitTests.ScenarioTests
         public void TearDown()
         {
         }
+
+        [Test]
+        public void HackathonScenario_Constructor_0_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+
+            // Assert
+
+
+            // Act
+            var result = EngineViewModel;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+        [Test]
+        public async Task HackathonScenario_Scenario_1_Default_Should_Pass()
+        {
+            /* 
+            * Scenario Number:  
+            *      1
+            *      
+            * Description: 
+            *      Make a Character called Yoshi, who dies in the first round
+            * 
+            * Changes Required (Classes, Methods etc.)  List Files, Methods, and Describe Changes: 
+            *      No Code changes requied 
+            * 
+            * Test Algrorithm:
+            *      Create Character named Yoshi
+            *      Set speed to -1 so he is really slow
+            *      Set Max health to 1 so he is weak
+            *      Set Current Health to 1 so he is weak
+            *  
+            *      Startup Battle
+            *      Run Auto Battle
+            * 
+            * Test Conditions:
+            *      Default condition is sufficient
+            * 
+            * Validation:
+            *      Verify Battle Returned True
+            *      Verify Yoshi is not in the Player List
+            *      Verify Round Count is 1
+            *  
+            */
+
+            //Arrange
+
+            // Set Character Conditions
+
+            EngineViewModel.Engine.MaxNumberCharacters = 1;
+
+            var CharacterPlayerYoshi = new CharacterModel
+                            {
+                                Speed = -1, // Will go last...
+                                Level = 1,
+                                CurrentHealth = 1,
+                                TotalExperience = 1,
+                                Name = "Yoshi"
+                            };
+
+            EngineViewModel.Engine.CharacterList.Add(CharacterPlayerYoshi);
+
+            // Set Monster Conditions
+
+            // Auto Battle will add the monsters
+
+
+            //Act
+            var result = await AutoBattleEngine.RunAutoBattle();
+
+            //Reset
+
+            //Assert
+            Assert.AreEqual(true, result);
+            Assert.AreEqual(null, AutoBattleEngine.PlayerList.Find(m => m.Name.Equals("Yoshi")));
+            Assert.AreEqual(1, AutoBattleEngine.Score.RoundCount);
+        }
     }
 }
