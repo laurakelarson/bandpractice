f2e256a0d327fab3ae2dd0eeb303d8d4ecb21d48
Laura Larson <larsonlaura@seattleu.edu>
2020-03-09 10:41:28 -0700

    Added fields and setup/teardown to hackathon scenario tests

--- UnitTests/ScenarioTests/Hackathon.cs
+++ UnitTests/ScenarioTests/Hackathon.cs
@@ -1,10 +1,32 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+
+using Game.Engine;
+using Game.Models;
+using System.Threading.Tasks;
+using Game.Helpers;
+using System.Linq;
+using Game.ViewModels;
 
 namespace UnitTests.ScenarioTests
 {
+    [TestFixture]
     class Hackathon
     {
+        BattleEngineViewModel EngineViewModel = BattleEngineViewModel.Instance;
+
+        AutoBattleEngine AutoBattleEngine;
+        BattleEngine BattleEngine;
+
+        [SetUp]
+        public void Setup()
+        {
+            AutoBattleEngine = EngineViewModel.AutoBattleEngine;
+            BattleEngine = EngineViewModel.Engine;
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+        }
     }
 }
