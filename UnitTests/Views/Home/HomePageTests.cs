7737878743edb6f78e3ce84443fca2f7d6ef36e5
Laura Larson <larsonlaura@seattleu.edu>
2020-03-10 19:11:59 -0700

    Added class HomePageTests to UT

---
+++ UnitTests/Views/Home/HomePageTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+
+namespace UnitTests.Views
+{
+    [TestFixture]
+    public class HomePageTests
+    {
+        App app;
+        HomePage page;
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new HomePage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        
+    }
+}
