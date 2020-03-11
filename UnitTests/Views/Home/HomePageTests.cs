453ad7ae02cd06ef59d4a8f0cb52a1e20ffc5351
Laura Larson <larsonlaura@seattleu.edu>
2020-03-10 19:17:14 -0700

    Added test HomePage_DemoTapeButton_Clicked_Default_Should_Pass()

--- UnitTests/Views/Home/HomePageTests.cs
+++ UnitTests/Views/Home/HomePageTests.cs
@@ -90,5 +90,18 @@ namespace UnitTests.Views
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void HomePage_DemoTapeButton_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+            // Act
+            page.Demotape_Button_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
