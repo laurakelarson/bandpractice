2c67fc4ffbee8bc6c766b3ce752d6c879400499a
Laura Larson <larsonlaura@seattleu.edu>
2020-03-10 19:12:49 -0700

    Added test HomePage_Constructor_Default_Should_Pass()

--- UnitTests/Views/Home/HomePageTests.cs
+++ UnitTests/Views/Home/HomePageTests.cs
@@ -38,6 +38,20 @@ namespace UnitTests.Views
             Application.Current = null;
         }
 
+        [Test]
+        public void HomePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
         
     }
 }
