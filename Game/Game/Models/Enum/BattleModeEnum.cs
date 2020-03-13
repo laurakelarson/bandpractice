5f5d7d92a298f9f4399f1f0ccfcb87a69ef3f6bd
Laura Larson <larsonlaura@seattleu.edu>
2020-03-12 18:05:18 -0700

    Added BattleModeEnum class

---
+++ Game/Game/Models/Enum/BattleModeEnum.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    /// <summary>
+    /// The Conditions a round can have
+    /// </summary>
+    public enum BattleModeEnum
+    {
+        // Invalid State
+        Unknown = 0,
+
+        // Default, just click until the end
+        SimpleNext = 1,
+
+        // Allow user to choose ability instead of action
+        SimpleAbility = 2,
+
+        // Map that just clicks until the end, monsters move, players don't move
+        MapNext = 3,
+
+        // Map that allows characters to choose ability, monsters move, players don't ove
+        MapAbility = 4,
+
+        // Map that allows characters to move and choose ability
+        MapFull = 5,
+    }
+
+    /// <summary>
+    /// Friendly strings for the Enum Class
+    /// </summary>
+    public static class BattleModeEnumExtensions
+    {
+        /// <summary>
+        /// Display a String for the Enums
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public static string ToMessage(this BattleModeEnum value)
+        {
+            // Default String
+            var Message = "Unknown";
+
+            switch (value)
+            {
+
+                case BattleModeEnum.MapFull:
+                    Message = "Map All Actions";
+                    break;
+
+                case BattleModeEnum.MapNext:
+                    Message = "Map Next Button";
+                    break;
+
+                case BattleModeEnum.MapAbility:
+                    Message = "Map Abilities";
+                    break;
+
+                case BattleModeEnum.SimpleNext:
+                    Message = "Simple Next";
+                    break;
+
+                case BattleModeEnum.SimpleAbility:
+                    Message = "Simple Abilities";
+                    break;
+
+                case BattleModeEnum.Unknown:
+                default:
+                    break;
+            }
+            return Message;
+        }
+    }
+}
