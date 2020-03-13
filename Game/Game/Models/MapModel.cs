764754788dc76171d24b4e670bb4a57bd24b3c9a
cbrod <broderickcel@seattleu.edu>
2020-03-12 17:28:45 -0700

    MapModel: add method ReturnClosestEmptyLocation

--- Game/Game/Models/MapModel.cs
+++ Game/Game/Models/MapModel.cs
@@ -250,6 +250,31 @@ namespace Game.Models
         }
 
         /// <summary>
+        /// Walk the Map and Find the Location that is close to the target
+        /// </summary>
+        /// <param name="Target"></param>
+        /// <returns></returns>
+        public MapModelLocation ReturnClosestEmptyLocation(MapModelLocation Target)
+        {
+            MapModelLocation Result = null;
+
+            int LowestDistance = int.MaxValue;
+
+            foreach (var data in GetEmptyLocations())
+            {
+                var distance = CalculateDistance(data, Target);
+                if (distance < LowestDistance)
+                {
+                    Result = data;
+                    LowestDistance = distance;
+                }
+            }
+
+
+            return Result;
+        }
+
+        /// <summary>
         /// See if the Attacker is next to the Defender by the distance of Range
         /// 
         /// If either the X or Y distance is less than or equal the range, then they can hit
