9c6b3404092c13dd88bf1fbb2cf7f38b74a6791c
Laura Larson <larsonlaura@seattleu.edu>
2020-03-12 18:06:10 -0700

    Added BattleActionEnum class

---
+++ Game/Game/Models/Enum/BattleActionEnum.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Game.Models.Enum
+{
+    /// <summary>
+    /// Actions that can happen in the battle.
+    /// 
+    /// Example.  
+    /// PlayerAlwaysHit can be Default, On, or Off
+    /// PlayerAbility can be Default, On, Off
+    /// </summary>
+    public enum BattleActionEnum
+    {
+        // Default behavior
+        Default = 0,
+
+        // Supress Behavior
+        Off = 2,
+
+        // Force behavior
+        On = 3,
+    }
+}
