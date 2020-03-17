7808b6e558cdd66064a7a343bbe3dcfc80c6b75a
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 13:19:27 -0700

    Added test ScoreCreatePage_Save_Clicked_Default_Should_Pass()

--- UnitTests/Views/Scores/ScoreCreatePageTests.cs
+++ UnitTests/Views/Scores/ScoreCreatePageTests.cs
@@ -71,6 +71,20 @@ namespace UnitTests.Views.Scores
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void ScoreCreatePage_Save_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
 
     }
 }
