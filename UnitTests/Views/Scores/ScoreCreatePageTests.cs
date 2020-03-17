63c64b59b94f306b0e667f32833d9bbca638ed39
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 13:18:02 -0700

    Added setup and teardown for score create page UT

--- UnitTests/Views/Scores/ScoreCreatePageTests.cs
+++ UnitTests/Views/Scores/ScoreCreatePageTests.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
 
 namespace UnitTests.Views.Scores
 {
-    class ScoreCreatePageTests
+    [TestFixture]
+    public class ScoreCreatePageTests : ScoreCreatePage
     {
+        App app;
+        ScoreCreatePage page;
+
+        public ScoreCreatePageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new ScoreCreatePage(new GenericViewModel<ScoreModel>(new ScoreModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
     }
 }
