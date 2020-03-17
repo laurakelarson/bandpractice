c6473685a44d67d5c2399d0f51969988cbef0334
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 13:45:14 -0700

    Added test ScoreUpdatePage_Constructor_Default_Should_Pass()

--- UnitTests/Views/Scores/ScoreUpdatePageTests.cs
+++ UnitTests/Views/Scores/ScoreUpdatePageTests.cs
@@ -43,6 +43,18 @@ namespace UnitTests.Views.Scores
             Application.Current = null;
         }
 
+        [Test]
+        public void ScoreUpdatePage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
 
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
     }
 }
