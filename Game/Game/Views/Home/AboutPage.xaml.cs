d443081c43d39db2adfbeb9328202fecfcb9459f
Alex Peterson <petersonalex@seattleu.edu>
2020-03-16 20:37:57 -0700

    Add method 'GetItemsPost()' to About Page code behind.

--- Game/Game/Views/Home/AboutPage.xaml.cs
+++ Game/Game/Views/Home/AboutPage.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.ComponentModel;
 using System.Threading.Tasks;
 using Game.Helpers;
+using Game.Models;
+using Game.Services;
 using Game.ViewModels;
 using Xamarin.Forms;
 
@@ -165,6 +168,49 @@ namespace Game.Views
             {
                 // Add them line by one, use \n to force new line for output display.
                 // Build up the output string by adding formatted ItemModel Output
+                result += ItemModel.FormatOutput() + "\n";
+            }
+
+            return result;
+        }
+
+        /// <summary>
+        /// Get Items using the HTTP Post command
+        /// </summary>
+        /// <returns></returns>
+        public async Task<string> GetItemsPost()
+        {
+            var result = "No Results";
+            var dataList = new List<ItemModel>();
+
+            var number = Convert.ToInt32(ServerItemValue.Text);
+            var level = 6;  // Max Value of 6
+            var attribute = AttributeEnum.Unknown;  // Any Attribute
+            var location = ItemLocationEnum.Unknown;    // Any Location
+            var random = true;  // Random between 1 and Level
+            var updateDataBase = true;  // Add them to the DB
+            var category = 0;   // What category to filter down to, 0 is all
+
+            // will return shoes value 10 of speed.
+            // Example  result = await ItemsController.Instance.GetItemsFromGame(1, 10, AttributeEnum.Speed, ItemLocationEnum.Feet, false, true);
+            dataList = await ItemService.GetItemsFromServerPostAsync(number, level, attribute, location, category, random, updateDataBase);
+
+            if (dataList == null)
+            {
+                return result;
+            }
+
+            if (dataList.Count == 0)
+            {
+                return result;
+            }
+
+            // Reset the output
+            result = "";
+
+            foreach (var ItemModel in dataList)
+            {
+                // Add them line by one, use \n to force new line for output display.
                 result += ItemModel.FormatOutput() + "\n";
             }
 
