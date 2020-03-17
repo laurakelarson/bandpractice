29bd1171925f4289fe049ce8a075276fae2f04fd
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 12:42:48 -0700

    Added test EncyclopediaPage_Constructor_Default_Should_Pass()

--- UnitTests/Views/Encyclopedia/EncyclopediaPageTests.cs
+++ UnitTests/Views/Encyclopedia/EncyclopediaPageTests.cs
@@ -20,6 +20,39 @@ namespace UnitTests.Views.Encyclopedia
         App app;
         EncyclopediaPage page;
 
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new EncyclopediaPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+        [Test]
+        public void EncyclopediaPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
 
     }
 }
