12f53df03a3e5a5dc32c28a52b4ac90bd4b55252
Alex Peterson <petersonalex@seattleu.edu>
2020-03-16 19:30:51 -0700

    Add method 'ConvertFromJson()' to ItemModelJsonHelper.

--- Game/Game/Helpers/ItemModelJsonHelper.cs
+++ Game/Game/Helpers/ItemModelJsonHelper.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Game.Models;
+using Newtonsoft.Json.Linq;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +8,32 @@ namespace Game.Helpers
 {
     public static class ItemModelJsonHelper
     {
+        /// <summary>
+        /// Converts a single object that is a json string into a single ItemModel
+        /// </summary>
+        /// <param name="json"></param>
+        /// <returns></returns>
+        public static ItemModel ConvertFromJson(JObject json)
+        {
+            var myData = new ItemModel();
+
+            myData.Name = JsonHelper.GetJsonString(json, "Name");
+            myData.Guid = JsonHelper.GetJsonString(json, "Guid");
+            myData.Id = myData.Guid;    // Set to be the same as Guid, does not come down from server, but needed for DB
+
+            myData.Description = JsonHelper.GetJsonString(json, "Description");
+            myData.ImageURI = JsonHelper.GetJsonString(json, "ImageURI");
+
+            myData.Value = JsonHelper.GetJsonInteger(json, "Value");
+            myData.Range = JsonHelper.GetJsonInteger(json, "Range");
+            myData.Damage = JsonHelper.GetJsonInteger(json, "Damage");
+
+            //myData.Category = JsonHelper.GetJsonInteger(json, "Category");
+
+            myData.Location = (ItemLocationEnum)JsonHelper.GetJsonInteger(json, "Location");
+            myData.Attribute = (AttributeEnum)JsonHelper.GetJsonInteger(json, "Attribute");
+
+            return myData;
+        }
     }
 }
