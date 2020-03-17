bf57e08a760f956340a66a286d91d95163dd4188
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 13:44:53 -0700

    Added setup and teardown for ScoreUpdatePage UT

--- UnitTests/Views/Scores/ScoreUpdatePageTests.cs
+++ UnitTests/Views/Scores/ScoreUpdatePageTests.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
 
 namespace UnitTests.Views.Scores
 {
-    class ScoreUpdatePageTests
+    [TestFixture]
+    public class ScoreUpdatePageTests : ScoreUpdatePage
     {
+        App app;
+        ScoreUpdatePage page;
+
+        public ScoreUpdatePageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new ScoreUpdatePage(new GenericViewModel<ScoreModel>(new ScoreModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+
     }
 }
