eb436ce591fd2aa5064eb9132536a82aab650b58
Alex Peterson <petersonalex@seattleu.edu>
2020-03-16 18:11:26 -0700

    Add method 'SetHttpClient() to HttpClientService.

--- Game/Game/Services/HttpClientService.cs
+++ Game/Game/Services/HttpClientService.cs
@@ -42,5 +42,19 @@ namespace Game.Services
                 return _instance;
             }
         }
+
+        /// <summary>
+        /// Set the client
+        /// 
+        /// Used by UT to swap out clients for testing
+        /// 
+        /// </summary>
+        /// <param name="httpClient"></param>
+        /// <returns></returns>
+        public HttpClient SetHttpClient(HttpClient httpClient)
+        {
+            _httpClientInstance = httpClient;
+            return _httpClientInstance;
+        }
     }
 }
