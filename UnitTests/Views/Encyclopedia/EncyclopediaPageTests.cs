0ebb2297149f74b2e2e8bd0636324a4d3660bbc2
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 12:41:55 -0700

    Added Encyclopedia Page tests to UT

---
+++ UnitTests/Views/Encyclopedia/EncyclopediaPageTests.cs
@@ -0,0 +1,25 @@
+﻿
+using NUnit.Framework;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Xamarin.Forms.Mocks;
+using Xamarin.Forms;
+
+namespace UnitTests.Views.Encyclopedia
+{
+    [TestFixture]
+    class EncyclopediaPageTests
+    {
+        App app;
+        EncyclopediaPage page;
+
+
+    }
+}
