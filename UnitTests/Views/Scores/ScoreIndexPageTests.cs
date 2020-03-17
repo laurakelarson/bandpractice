d0615d74fc98b16f6fb20123761f909ba577f283
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 13:30:43 -0700

    Added test ScoreIndexPage_OnBackButtonPressed_Valid_Should_Pass()

--- UnitTests/Views/Scores/ScoreIndexPageTests.cs
+++ UnitTests/Views/Scores/ScoreIndexPageTests.cs
@@ -66,6 +66,18 @@ namespace UnitTests.Views.Scores
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void ScoreIndexPage_OnBackButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
 
+            // Act
+            OnBackButtonPressed();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
