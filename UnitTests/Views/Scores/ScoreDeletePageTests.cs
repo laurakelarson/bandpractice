e336e374b5a5c59184288cd85260d74e19940af3
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 12:57:38 -0700

    Added setup and teardown to ScoreDeletePage UT

--- UnitTests/Views/Scores/ScoreDeletePageTests.cs
+++ UnitTests/Views/Scores/ScoreDeletePageTests.cs
@@ -1,10 +1,46 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
 
 namespace UnitTests.Views.Scores
 {
-    class ScoreDeletePageTests
+    [TestFixture]
+    public class ScoreDeletePageTests : ScoreDeletePage
     {
+        App app;
+        ScoreDeletePage page;
+
+        public ScoreDeletePageTests() : base(new GenericViewModel<ScoreModel>(new ScoreModel())) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new ScoreDeletePage(new GenericViewModel<ScoreModel>(new ScoreModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
     }
 }
