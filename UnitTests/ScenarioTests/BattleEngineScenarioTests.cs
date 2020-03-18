afff96c609d6d6a3a2b21ec9d623875bc396a89c
cbrod <broderickcel@seattleu.edu>
2020-03-17 22:01:24 -0700

    Add test BattleEngine_Constructor_Default_Should_Pass()

--- UnitTests/ScenarioTests/BattleEngineScenarioTests.cs
+++ UnitTests/ScenarioTests/BattleEngineScenarioTests.cs
@@ -25,12 +25,19 @@ namespace UnitTests.ScenarioTests
         {
         }
 
+        //  test constructor
         [Test]
-        public void TestMethod()
+        public void BattleEngine_Constructor_Default_Should_Pass()
         {
-            // TODO: Add your test code here
-            var answer = 42;
-            Assert.That(answer, Is.EqualTo(42), "Some useful error message");
+            // Arrange
+
+            // Act
+            var result = Engine;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
         }
     }
 }
