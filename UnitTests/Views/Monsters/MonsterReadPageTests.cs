b959635f24c1d0bc2a4158258cdf396dedddd32c
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 19:02:03 -0700

    Added test MonsterReadPage_OnBackButtonPressed_Valid_Should_Pass()

--- UnitTests/Views/Monsters/MonsterReadPageTests.cs
+++ UnitTests/Views/Monsters/MonsterReadPageTests.cs
@@ -81,5 +81,21 @@ namespace UnitTests.Views.Monsters
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void MonsterReadPage_OnBackButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            OnBackButtonPressed();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
     }
 }
