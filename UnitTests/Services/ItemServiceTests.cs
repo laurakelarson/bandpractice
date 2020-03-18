d1c2d3f6a3145e934180d7a6d261eb67c4f069d4
Alex Peterson <petersonalex@seattleu.edu>
2020-03-17 21:39:56 -0700

    Add test for get request of group 10 items from service.

--- UnitTests/Services/ItemServiceTests.cs
+++ UnitTests/Services/ItemServiceTests.cs
@@ -80,5 +80,30 @@ namespace UnitTests.Services
             // Assert
             Assert.AreEqual(true, result.Count == 1);
         }
+
+        // Test get request for group 10 items from service 
+        [Test]
+        public async Task ItemService_GetItemsFromServerPostAsync_Valid_10_Should_Pass()
+        {
+            // Arrange
+            var number = 10;
+
+            var level = 6;  // Max Value of 6
+            var attribute = AttributeEnum.Unknown;  // Any Attribute
+            var location = ItemLocationEnum.Unknown;    // Any Location
+            var random = true;  // Random between 1 and Level
+            var updateDataBase = true;  // Add them to the DB
+            var category = 0;   // What category to filter down to, 0 is all
+
+            // will return shoes value 10 of speed.
+
+            // Act
+            var result = await ItemService.GetItemsFromServerPostAsync(number, level, attribute, location, category, random, updateDataBase);
+
+            // Reset
+
+            // Assert
+            Assert.AreEqual(true, result.Count == 10);
+        }
     }
 }
