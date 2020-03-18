3f7910778aded21f1c938817fa178dc29bda5c2a
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:42:57 -0700

    Added test CharacterCreatePage_Save_Clicked_Null_Image_Should_Pass()

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -86,6 +86,19 @@ namespace UnitTests.Views.Characters
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterCreatePage_Save_Clicked_Null_Image_Should_Pass()
+        {
+            // Arrange
+            page.ViewModel.Data.ImageURI = null;
 
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
