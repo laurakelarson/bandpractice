8f81920b0d5a9fdf40ece947a86cd9471ed9e0a5
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:42:12 -0700

    Added test CharacterCreatePage_Cancel_Clicked_Default_Should_Pass()

--- UnitTests/Views/Characters/CharacterCreatePageTests.cs
+++ UnitTests/Views/Characters/CharacterCreatePageTests.cs
@@ -57,5 +57,21 @@ namespace UnitTests.Views.Characters
             // Assert
             Assert.IsNotNull(result);
         }
+
+        [Test]
+        public void CharacterCreatePage_Cancel_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Cancel_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
     }
 }
