f2ba2eaea4f1ce525d41a931fef9ab9c84596619
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:11:08 -0700

    Added setup and teardown for ItemReadPage UT

--- Game/Game/Views/Items/ItemReadPage.xaml.cs
+++ Game/Game/Views/Items/ItemReadPage.xaml.cs
@@ -13,7 +13,10 @@ namespace Game.Views
     public partial class ItemReadPage : ContentPage
     {
         // View Model for Item
-        readonly GenericViewModel<ItemModel> ViewModel;
+        public readonly GenericViewModel<ItemModel> ViewModel;
+
+        // Empty Constructor for UTs
+        public ItemReadPage(bool UnitTest) { }
 
         /// <summary>
         /// Constructor called with a view model
@@ -33,7 +36,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        async void Update_Clicked(object sender, EventArgs e)
+        public async void Update_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new ItemUpdatePage(new GenericViewModel<ItemModel>(ViewModel.Data))));
             await Navigation.PopAsync();
@@ -44,7 +47,7 @@ namespace Game.Views
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-        async void Delete_Clicked(object sender, EventArgs e)
+        public async void Delete_Clicked(object sender, EventArgs e)
         {
             await Navigation.PushModalAsync(new NavigationPage(new ItemDeletePage(new GenericViewModel<ItemModel>(ViewModel.Data))));
             await Navigation.PopAsync();
--- UnitTests/Views/Items/ItemReadPageTests.cs
+++ UnitTests/Views/Items/ItemReadPageTests.cs
@@ -1,10 +1,48 @@
-﻿using System;
+﻿using NUnit.Framework;
+using System;
+using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
+
+using Game;
+using Game.Views;
+using Game.ViewModels;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
 
 namespace UnitTests.Views.Items
 {
-    class ItemReadPageTests
+    [TestFixture]
+    public class ItemReadPageTests : ItemReadPage
     {
+        App app;
+        ItemReadPage page;
+
+        public ItemReadPageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new ItemReadPage(new GenericViewModel<ItemModel>(new ItemModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+
     }
 }
