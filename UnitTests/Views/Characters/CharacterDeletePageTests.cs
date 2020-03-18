378604026116ff2f2b6449c2cbf060d18d904e8f
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:26:20 -0700

    Added test CharacterDeletePage_Delete_Clicked_Default_Should_Pass()

--- UnitTests/Views/Characters/CharacterDeletePageTests.cs
+++ UnitTests/Views/Characters/CharacterDeletePageTests.cs
@@ -72,6 +72,18 @@ namespace UnitTests.Views.Characters
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterDeletePage_Delete_Clicked_Default_Should_Pass()
+        {
+            // Arrange
 
+            // Act
+            page.Delete_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
