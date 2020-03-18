c68438721349d7f74b0f65a8ecb1d7bb0e5f4361
Alex Peterson <petersonalex@seattleu.edu>
2020-03-17 21:07:21 -0700

    Add test for valid json and url to HttpClientServiceTests.

--- UnitTests/Services/HttpClientServiceTests.cs
+++ UnitTests/Services/HttpClientServiceTests.cs
@@ -221,6 +221,55 @@ namespace UnitTests.Services
             Assert.AreEqual(null, result);
         }
 
+        // Test valid url and json 
+        [Test]
+        public async Task HttpClientService_GetJsonPostAsync_Valid_Moq_Should_Pass()
+        {
+            // Arrange
+
+            var MockHttpClient = new HttpClient(new MockHttpMessageHandler());
+
+            var RestUrl = "http://some.fake.url";
+
+            var OldHttpClient = Service.GetHttpClient();
+            Service.SetHttpClient(MockHttpClient);
+
+            ResponseMessage.SetResponseMessageStringContent(ResponseMessage.GetStringContent);
+
+            int number = 0;
+            int level = 0;
+            AttributeEnum attribute = AttributeEnum.Attack;
+            ItemLocationEnum location = ItemLocationEnum.Feet;
+            int category = 0;
+            bool random = false;
+
+            var dict = new Dictionary<string, string>
+            {
+                { "Number", number.ToString()},
+                { "Level", level.ToString()},
+                { "Attribute", ((int)attribute).ToString()},
+                { "Location", ((int)location).ToString()},
+                { "Random", random.ToString()},
+                { "Category", category.ToString()},
+            };
+
+            // Convert parameters to a key value pairs to a json object
+            JObject finalContentJson = (JObject)JToken.FromObject(dict);
+
+            // Act
+            var result = await Service.GetJsonPostAsync(RestUrl, finalContentJson);
+
+            // Parse them
+            var resultList = ItemModelJsonHelper.ParseJson(result);
+
+            // Reset
+            Service.SetHttpClient(OldHttpClient);
+            ResponseMessage.ResetResponseMessageStringContent();
+
+            // Assert
+            Assert.AreEqual(2, resultList.Count);
+        }
+
         // ResponseMessage class used for sending mock responses 
         public static class ResponseMessage
         {
