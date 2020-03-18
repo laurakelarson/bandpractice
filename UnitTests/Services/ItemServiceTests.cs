0e3bb08f1cfed0e03aa47837d59f2a837069a267
Alex Peterson <petersonalex@seattleu.edu>
2020-03-17 21:35:40 -0700

    Add test for constructor to ItemServiceTests.

--- UnitTests/Services/ItemServiceTests.cs
+++ UnitTests/Services/ItemServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using Game.Services;
+using NUnit.Framework;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -22,5 +23,22 @@ namespace UnitTests.Services
         {
             await Game.Helpers.DataSetsHelper.WipeData();
         }
+
+        // Test Contstructor
+        [Test]
+        public void ItemService_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = ItemService.DefaultImageURI;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
     }
 }
