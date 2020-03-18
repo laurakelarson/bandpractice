e1952e0e14d9dac8d770f6a6a4f3676de42051cd
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 20:30:13 -0700

    Added test ItemCreatePage_OnBackButtonPressed_Valid_Should_Pass()

--- UnitTests/Views/Items/ItemCreatePageTests.cs
+++ UnitTests/Views/Items/ItemCreatePageTests.cs
@@ -101,6 +101,18 @@ namespace UnitTests.Views.Items
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void ItemCreatePage_OnBackButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
 
+            // Act
+            OnBackButtonPressed();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
