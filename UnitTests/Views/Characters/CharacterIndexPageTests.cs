92e4a7a7a1ba2cdaeead2e8312ac6187a992f13f
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:30:54 -0700

    Added test CharacterIndexPage_Constructor_Default_Should_Pass()

--- UnitTests/Views/Characters/CharacterIndexPageTests.cs
+++ UnitTests/Views/Characters/CharacterIndexPageTests.cs
@@ -39,5 +39,20 @@ namespace UnitTests.Views.Characters
             Application.Current = null;
         }
 
+        [Test]
+        public void CharacterIndexPage_Constructor_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            var result = page;
+
+            // Reset
+
+            // Assert
+            Assert.IsNotNull(result);
+        }
+
+
     }
 }
