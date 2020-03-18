7993b77e357245f64b83cb75b85c4531805e6341
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:20:33 -0700

    Added test ItemUpdatePage_OnBackButtonPressed_Valid_Should_Pass()

--- UnitTests/Views/Items/ItemUpdatePageTests.cs
+++ UnitTests/Views/Items/ItemUpdatePageTests.cs
@@ -100,6 +100,18 @@ namespace UnitTests.Views.Items
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void ItemUpdatePage_OnBackButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
 
+            // Act
+            OnBackButtonPressed();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
