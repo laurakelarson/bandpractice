e18821791725ed2a8c29efbb53837b25a815114a
Alex Peterson <petersonalex@seattleu.edu>
2020-03-17 21:30:40 -0700

    Add setup and teardown to ItemServiceTests.

--- UnitTests/Services/ItemServiceTests.cs
+++ UnitTests/Services/ItemServiceTests.cs
@@ -2,11 +2,25 @@
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace UnitTests.Services
 {
     [TestFixture]
     public class ItemServiceTests
     {
+        // Setup
+        [SetUp]
+        public void Setup()
+        {
+            Game.Helpers.DataSetsHelper.WarmUp();
+        }
+
+        // Tear down
+        [TearDown]
+        public async Task TearDown()
+        {
+            await Game.Helpers.DataSetsHelper.WipeData();
+        }
     }
 }
