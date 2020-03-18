33b40e77a9b547ee4bd366c360f6fa555552f85f
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 18:46:31 -0700

    Added setup and teardown to monsterdeletepage UT

--- UnitTests/Views/Monsters/MonsterDeletePageTests.cs
+++ UnitTests/Views/Monsters/MonsterDeletePageTests.cs
@@ -7,11 +7,37 @@ using Game.Models;
 
 using Xamarin.Forms;
 using Xamarin.Forms.Mocks;
+using Game.Views.Monsters;
 
 namespace UnitTests.Views.Monsters
 {
     [TestFixture]
     public class MonsterDeletePageTests : MonsterDeletePage
     {
+        App app;
+        MonsterDeletePage page;
+
+        public MonsterDeletePageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new MonsterDeletePage(new GenericViewModel<MonsterModel>(new MonsterModel()));
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
+
     }
 }
