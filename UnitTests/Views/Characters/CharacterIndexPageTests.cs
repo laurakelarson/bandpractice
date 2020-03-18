0840fddd5e2db124f62a9f4562895216c6d53648
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:29:35 -0700

    Added setup and teardown to CharacterIndexPage UT

--- UnitTests/Views/Characters/CharacterIndexPageTests.cs
+++ UnitTests/Views/Characters/CharacterIndexPageTests.cs
@@ -1,10 +1,43 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using NUnit.Framework;
+
+using Game;
+using Game.Views;
+using Game.Models;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Mocks;
+using Game.ViewModels;
+using System.Threading.Tasks;
+using Game.Views.Characters;
 
 namespace UnitTests.Views.Characters
 {
-    class CharacterIndexPageTests
+    [TestFixture]
+    public class CharacterIndexPageTests : CharacterIndexPage
     {
+        App app;
+        CharacterIndexPage page;
+
+        public CharacterIndexPageTests() : base(true) { }
+
+        [SetUp]
+        public void Setup()
+        {
+            // Initilize Xamarin Forms
+            MockForms.Init();
+
+            //This is your App.xaml and App.xaml.cs, which can have resources, etc.
+            app = new App();
+            Application.Current = app;
+
+            page = new CharacterIndexPage();
+        }
+
+        [TearDown]
+        public void TearDown()
+        {
+            Application.Current = null;
+        }
+
     }
 }
