0187b55ee00b735e6e13ebbe550f4121842fa33e
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:55:57 -0700

    Added test CharacterUpdatePage_Save_Clicked_Default_Should_Pass()

--- UnitTests/Views/Characters/CharacterUpdatePageTests.cs
+++ UnitTests/Views/Characters/CharacterUpdatePageTests.cs
@@ -68,6 +68,18 @@ namespace UnitTests.Views.Characters
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void CharacterUpdatePage_Save_Clicked_Default_Should_Pass()
+        {
+            // Arrange
 
+            // Act
+            page.Save_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
     }
 }
