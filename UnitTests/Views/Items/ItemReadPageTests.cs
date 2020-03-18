1e133ac1990457d3c892d042373e4f09109ecc86
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:13:00 -0700

    Added test ItemReadPage_Delete_Clicked_Default_Should_Pass()

--- UnitTests/Views/Items/ItemReadPageTests.cs
+++ UnitTests/Views/Items/ItemReadPageTests.cs
@@ -71,6 +71,21 @@ namespace UnitTests.Views.Items
             Assert.IsTrue(true); // Got to here, so it happened...
         }
 
+        [Test]
+        public void ItemReadPage_Delete_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Delete_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
 
     }
 }
