043d13f55af63557829f07a3f078723a76438d1d
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 22:02:45 -0700

    Added test CharacterReadPage_OnBackButtonPressed_Valid_Should_Pass()

--- UnitTests/Views/Characters/CharacterReadPageTests.cs
+++ UnitTests/Views/Characters/CharacterReadPageTests.cs
@@ -81,5 +81,21 @@ namespace UnitTests.Views.Characters
             // Assert
             Assert.IsTrue(true); // Got to here, so it happened...
         }
+
+        [Test]
+        public void CharacterReadPage_OnBackButtonPressed_Valid_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            OnBackButtonPressed();
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
+
     }
 }
