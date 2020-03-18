d86716e53d2ebd41dd4e2216be05ba5e1ea79fb3
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 18:26:04 -0700

    Added test MonsterCreatePage_Cancel_Clicked_Default_Should_Pass()

--- UnitTests/Views/Monsters/MonsterCreatePageTests.cs
+++ UnitTests/Views/Monsters/MonsterCreatePageTests.cs
@@ -52,6 +52,20 @@ namespace UnitTests.Views.Monsters
             Assert.IsNotNull(result);
         }
 
+        [Test]
+        public void MonsterCreatePage_Cancel_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Cancel_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
+
 
     }
 }
