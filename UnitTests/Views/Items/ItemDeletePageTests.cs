c255ccf6291787920c83e555fc78074b433a1255
Laura Larson <larsonlaura@seattleu.edu>
2020-03-17 21:00:22 -0700

    Added test ItemDeletePage_Cancel_Clicked_Default_Should_Pass()

--- UnitTests/Views/Items/ItemDeletePageTests.cs
+++ UnitTests/Views/Items/ItemDeletePageTests.cs
@@ -57,7 +57,19 @@ namespace UnitTests.Views.Items
             Assert.IsNotNull(result);
         }
 
-        
+        [Test]
+        public void ItemDeletePage_Cancel_Clicked_Default_Should_Pass()
+        {
+            // Arrange
+
+            // Act
+            page.Cancel_Clicked(null, null);
+
+            // Reset
+
+            // Assert
+            Assert.IsTrue(true); // Got to here, so it happened...
+        }
 
 
     }
